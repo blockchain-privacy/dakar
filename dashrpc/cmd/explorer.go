5a79ad7f0a86b07a62dba18ce625abb328da1ae6
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-05-13 15:10:12 +0200

    file logging and cli refactor

--- dashrpc/cmd/explorer.go
+++ dashrpc/cmd/explorer.go
@@ -4,58 +4,77 @@ import (
 	"dashrpc"
 	"dashrpc/rpcclient"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/dgraph-io/badger/v2"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"os"
 	"strconv"
 )
 
-//
-// This is the ENTRY point for the web-based REST API backend.
-//
-// Work in progress.
-//
+func setDefaultHeader(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept")
+}
 
-const (
-	// default server port
-	SERVER_PORT = 8081
-)
+// API pattern: "/"
+// OUTPUT: List of patterns
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+	log.Println("Accessed", r.URL.Path)
+	setDefaultHeader(w)
+	// not handling possible errors
+	_, e := fmt.Fprintln(w, "Possible routes:")
+	if e != nil {
+		log.Println(e)
+	}
 
+	_, e = fmt.Fprintln(w, "/\t\t-> This page")
+	if e != nil {
+		log.Println(e)
+	}
 
+	_, e = fmt.Fprintln(w, "/tx/<hash>\t-> Transaction details")
+	if e != nil {
+		log.Println(e)
+	}
+}
 
-//
 // handler builder
 //
 // API pattern: "/txt/<hash>"
 // OUTPUT: dashrpc.Transaction
-//
 func handlerTxDetails(db *badger.DB, client *rpcclient.Client) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r * http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Accessed", r.URL.Path)
+		setDefaultHeader(w)
 		txHashString := r.URL.Path[4:]
 		txDetails := dashrpc.TxDetails{}
 		err := dashrpc.DbGetTxDetails(db, txHashString, &txDetails)
 		if err != nil {
-			http.Error(w, err.Error() + " Key: "+txHashString, http.StatusNotFound)
+			http.Error(w, err.Error()+" Key: "+txHashString, http.StatusNotFound)
 			return
 		}
 		txHash, err := chainhash.NewHashFromStr(txHashString)
 		if err != nil {
-			http.Error(w, err.Error() + " Key: "+txHashString, http.StatusNotFound)
+			http.Error(w, err.Error()+" Key: "+txHashString, http.StatusNotFound)
 			return
 		}
 
 		tx, err := client.GetRawTransactionVerbose(txHash)
 		if err != nil {
-			http.Error(w, err.Error() + " Key: "+txHashString, http.StatusNotFound)
+			http.Error(w, err.Error()+" Key: "+txHashString, http.StatusNotFound)
 			return
 		}
 		block := dashrpc.Block{}
 		err = dashrpc.DbGetBlock(db, tx.BlockHash, &block)
 		if err != nil {
-			http.Error(w, err.Error() + " Block hash: "+tx.BlockHash, http.StatusNotFound)
+			http.Error(w, err.Error()+" Block hash: "+tx.BlockHash, http.StatusNotFound)
 			return
 		}
 		transaction := dashrpc.Transaction{}
@@ -65,17 +84,72 @@ func handlerTxDetails(db *badger.DB, client *rpcclient.Client) func(http.Respons
 		transaction.Confirmations = tx.Confirmations
 		transaction.Version = tx.Version
 		transaction.Tx = txDetails
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept")
 		// common['Access-Control-Request-Method'] = '*'")
 		err = json.NewEncoder(w).Encode(transaction)
 		if err != nil {
-			http.Error(w, err.Error() + " TxDetails: "+txDetails.String(), http.StatusInternalServerError)
+			http.Error(w, err.Error()+" TxDetails: "+txDetails.String(), http.StatusInternalServerError)
 		}
 	}
 }
 
+// same as buildEndpoint in crawler.go
+func buildExplorerEndpoint(rpcHost string, rpcPort uint) (string, error) {
+	// check if ip is valid
+	if ip := net.ParseIP(rpcHost); ip == nil {
+		return "", errors.New("IP is not valid")
+	}
+
+	// build endpoint string
+	return rpcHost + ":" + strconv.Itoa(int(rpcPort)), nil
+}
+
+type ExplorerCLIArguments struct {
+	badgerDir     string
+	rpcUser       string
+	rpcPassword   string
+	isPrintStatus bool
+	rpcEndpoint   string
+	logfile       string
+	serverPort    uint16 // type also defines max port number
+	err           error
+}
+
+func getExplorerCLIArgs() (cliArgs ExplorerCLIArguments) {
+	badgerDir := flag.String("db", "/tmp/badger", "Badger database location")
+	rpcUser := flag.String("rpcuser", "rpc1user", "Dash RPC user")
+	rpcPassword := flag.String("rpcpassword", "1234pass", "Dash RPC password")
+	rpcHost := flag.String("rpchost", "0.0.0.0", "Dash RPC host IP (default: 0.0.0.0)")
+	rpcPort := flag.Uint("rpcport", 9998, "Dash RPC port (default: 9998)")
+	serverPort := flag.Uint("serverport", 8081, "Explorer server port (default: 8081)")
+	isPrintStatus := flag.Bool("status", false, "Prints current processing status")
+	logfile := flag.String("logfile", "", "Specify log file (default: none)")
+	flag.Parse()
+
+	cliArgs.badgerDir = *badgerDir
+	cliArgs.rpcUser = *rpcUser
+	cliArgs.rpcPassword = *rpcPassword
+	cliArgs.isPrintStatus = *isPrintStatus
+	cliArgs.logfile = *logfile
+	cliArgs.serverPort = uint16(*serverPort)
+
+	ep, err := buildExplorerEndpoint(*rpcHost, *rpcPort)
+
+	if err != nil {
+		flag.PrintDefaults()
+		cliArgs.err = err
+		return cliArgs
+	}
+
+	cliArgs.rpcEndpoint = ep
+
+	if len(cliArgs.badgerDir) == 0 {
+		flag.PrintDefaults()
+		cliArgs.err = errors.New("error: badger dir not set")
+		return cliArgs
+	}
+
+	return cliArgs
+}
 
 //
 // Simple web-based utility to browse/lookup the TXs from the badger database
@@ -85,13 +159,30 @@ func handlerTxDetails(db *badger.DB, client *rpcclient.Client) func(http.Respons
 func main() {
 	fmt.Printf("Go DashRPC client  %s\nBlock explorer\n\n", dashrpc.VersionString)
 
-	badgerDir := flag.String("db", "/tmp/badger", "badger database location")
-	rpcUser := flag.String("rpcuser", "rpc1user", "Dash RPC user")
-	rpcPassword := flag.String("rpcpassword", "1234pass", "Dash RPC password")
-	isPrintStatus := flag.Bool("status", false, "prints current processing status")
+	cliArgs := getExplorerCLIArgs()
+	if cliArgs.err != nil {
+		fmt.Println(cliArgs.err)
+		return
+	}
 
-	flag.Parse()
-	db := dashrpc.SetupBadgerDB(*badgerDir)
+	// setup Logging
+	if len(cliArgs.logfile) > 0 {
+		f, err := os.OpenFile(cliArgs.logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("Error opening log file", err)
+			return
+		}
+		defer func() {
+			err = f.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
+		log.SetPrefix("explorer ")
+		log.SetOutput(io.MultiWriter(os.Stdout, f))
+	}
+
+	db := dashrpc.SetupBadgerDB(cliArgs.badgerDir)
 	defer func() {
 		e := db.Close()
 		if e != nil { /* ignore */
@@ -100,32 +191,34 @@ func main() {
 
 	// Setup the RPC connection
 	var conn = rpcclient.ConnConfig{
-		Host:       "0.0.0.0:9998",
-		User:       *rpcUser,
-		Pass:       *rpcPassword,
+		Host:       cliArgs.rpcEndpoint,
+		User:       cliArgs.rpcUser,
+		Pass:       cliArgs.rpcPassword,
 		DisableTLS: true,
 	}
 	client, err := rpcclient.New(&conn)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		log.Printf("Error: %v\n", err)
+		return
 	}
 
 	dbBlockCount := dashrpc.DbGetBlockCount(db)
 	dbTxCount := dashrpc.DbGetGlobalTxCount(db)
-	fmt.Printf("DB block count: %v  TX count: %v\n", dbBlockCount, dbTxCount)
-	if *isPrintStatus {
+	log.Printf("DB block count: %v  TX count: %v\n", dbBlockCount, dbTxCount)
+	if cliArgs.isPrintStatus {
 		dashrpc.PrintStatus(db)
 		return
 	}
 
 	var dbStatus string
 	dashrpc.DbGetStatus(db, &dbStatus)
-	fmt.Printf("DB status: %s\n", dbStatus)
+	log.Printf("DB status: %s\n", dbStatus)
 
 	// API end points
 	http.HandleFunc("/tx/", handlerTxDetails(db, client))
+	http.HandleFunc("/", handlerRoot)
 
 	// start the server
-	// TODO make the server port taken from the command-line parameters
-	log.Fatal(http.ListenAndServe(":" + strconv.FormatUint(SERVER_PORT, 10), nil))
+	log.Println("Starting server on port", cliArgs.serverPort)
+	log.Fatal(http.ListenAndServe(":"+strconv.FormatUint(uint64(cliArgs.serverPort), 10), nil))
 }
