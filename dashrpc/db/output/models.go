d8f531e7f3bd2d2ab3d68649aa683de06702efe6
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-07-20 18:43:05 +0200

    refactor output into own module

--- dashrpc/db/output/models.go
+++ dashrpc/db/output/models.go
@@ -1,1 +1,22 @@
 package output
+
+type Output struct {
+	Uid        string   `json:"uid,omitempty"`
+	Index      string   `json:"index,omitempty"`
+	TxType     string   `json:"txtype,omitempty"`
+	Amount     string   `json:"amount,omitempty"`
+	IsCoinbase string   `json:"iscoinbase,omitempty"`
+	DType      []string `json:"dgraph.type,omitempty"`
+}
+
+type outputQuery struct {
+	GetOutput []struct {
+		Transaction []struct {
+			Outputs []Output `json:"tx_outputs"`
+		} `json:"transaction"`
+	} `json:"getOutput"`
+}
+
+//type outputquery struct {
+//	Q []transaction.Transaction `json:"q"`
+//}
--- dashrpc/db/output/output.go
+++ dashrpc/db/output/output.go
@@ -1,10 +1,71 @@
 package output
 
-type Output struct {
-	Uid        string   `json:"uid,omitempty"`
-	Index      string   `json:"index,omitempty"`
-	TxType     string   `json:"txtype,omitempty"`
-	Amount     string   `json:"amount,omitempty"`
-	IsCoinbase string   `json:"iscoinbase,omitempty"`
-	DType      []string `json:"dgraph.type,omitempty"`
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/dgraph-io/dgo/v2"
+	"strconv"
+)
+
+// gets output information from the database
+func GetOutput(c *dgo.Dgraph, txHash string, index uint32, output *Output, isInput bool) error {
+	tx := c.NewReadOnlyTxn()
+	relationship := "tx_outputs"
+	if isInput {
+		relationship = "tx_inputs"
+	}
+
+	query := fmt.Sprintf(`query Q($hash: string, $idx: string) {
+				getOutput(func: eq(txhash, $hash)) {
+					%s @filter(eq(index, $idx)) {
+						uid
+						index
+						amount
+						txtype
+						iscoinbase
+						dgraph.type
+					}
+				}
+			  }
+				`, relationship)
+
+	vars := make(map[string]string)
+	vars["$hash"] = txHash
+
+	vars["$idx"] = strconv.FormatUint(uint64(index), 10)
+	resp, err := tx.QueryWithVars(context.Background(), query, vars)
+	if err != nil {
+		return err
+	}
+	var r outputQuery
+	err = json.Unmarshal(resp.Json, &r)
+
+	if err != nil {
+		return err
+	}
+
+	lenQ := len(r.GetOutput)
+	if lenQ == 0 {
+		return errors.New("no output found")
+	}
+
+	lenTx := len(r.GetOutput[0].Transaction)
+	if lenTx == 0 {
+		return errors.New("no output found")
+	}
+
+	lenO := len(r.GetOutput[0].Transaction[0].Outputs)
+	if lenO == 0 {
+		return errors.New("no output found")
+	}
+
+	*output = r.GetOutput[0].Transaction[0].Outputs[0]
+	if lenQ > 1 || lenTx > 1 || lenO > 1 {
+		// found more than one output, which should not be possible
+		return errors.New("found more than one output")
+	}
+
+	return nil
 }
