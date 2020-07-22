0210b6b0a8a501c287742eab0d79e153314c43b2
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-07-22 13:53:03 +0200

    add address String() function

--- dashrpc/db/address/models.go
+++ dashrpc/db/address/models.go
@@ -1,12 +1,25 @@
 package address
 
-import op "dashrpc/db/output"
+import (
+	op "dashrpc/db/output"
+	"fmt"
+)
 
 type Address struct {
 	Uid     string      `json:"uid,omitempty"`
 	Hash    string      `json:"addresshash,omitempty"`
 	Outputs []op.Output `json:"addr_outputs,omitempty"`
 	DType   []string    `json:"dgraph.type,omitempty"`
+}
+
+func (a Address) String() string {
+	output := fmt.Sprintf("Uid: %s, Hash: %s", a.Uid, a.Hash)
+
+	if a.Outputs != nil {
+		output += fmt.Sprintf(", OutputCount: %d", len(a.Outputs))
+	}
+
+	return output
 }
 
 // checks if the given address has all attributes filled
