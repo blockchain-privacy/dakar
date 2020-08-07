c21a36b9d077fccb4c3026a422562940f2e84e43
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-08-07 14:02:09 +0200

    only show if data is available

--- app/src/components/EntryView.vue
+++ app/src/components/EntryView.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container class="fill-height" fluid>
+  <v-container class="fill-height" fluid v-if="data">
     <v-row align="center" justify="center">
-      <v-col cols="12" sm="12" md="10" lg="8" xl="6">
+      <v-col cols="12" sm="12" md="10" lg="9" xl="8">
         <v-card class="elevation-12">
           <v-toolbar color="primary" dark flat>
             <v-toolbar-title>
