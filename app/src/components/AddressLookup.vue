8d359830a8594a1dd0d7596c3a14a9193984dc00
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-08-07 15:17:53 +0200

    redesign address view

--- app/src/components/AddressLookup.vue
+++ app/src/components/AddressLookup.vue
@@ -1,54 +1,58 @@
 <template>
-  <v-layout row v-if="data">
-    <v-flex xs12 sm8 offset-sm2>
-      <v-card>
-        <v-card-title>
-          <v-icon>mdi-bank-transfer</v-icon>
-          Address
-        </v-card-title>
-        <v-list two-line subheader>
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-icon>mdi-format-header-pound</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>Hash</v-list-item-title>
-              <v-list-item-subtitle>{{ data.addresshash }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item v-for="o in data.addr_outputs" v-bind:key="o.input_transaction + o.output_transaction">
-            <v-list-item-avatar>
-              <v-icon></v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>Output</v-list-item-title>
-              <v-list-item-subtitle>Amount: {{ o.amount }}</v-list-item-subtitle>
-              <v-list-item-subtitle v-if="o.iscoinbase" :data="o.iscoinbase">Coinbase:
-                {{ o.iscoinbase }}
-              </v-list-item-subtitle>
-              <v-list-item-subtitle>Output Transaction:
-                <router-link :to="o.output_transaction">{{ o.output_transaction }}</router-link>
-              </v-list-item-subtitle>
-              <v-list-item-subtitle v-if="o.input_transaction">Input transaction:
-                <router-link :to="o.input_transaction">
-                  {{ o.input_transaction }}
-                </router-link>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
-      <v-card><!-- TODO hack, to leave space at the bottom, such that content is not hidden by the footer.-->
-        <div style="height: 40pt">
-        </div>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container class="fill-height" fluid v-if="data">
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="12" md="10" lg="9" xl="8">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>
+              <v-icon>mdi-card-bulleted-outline</v-icon>
+              Address {{ data.addresshash }}
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col v-for="o in data.addr_outputs" v-bind:key="o.input_transaction + o.output_transaction">
+                  <v-list-item>
+                    <v-list-item-avatar>
+                      <v-icon>mdi-currency-usd-circle-outline</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                      <v-list-item-title>Output</v-list-item-title>
+                      <v-list-item-subtitle>Amount: {{ o.amount }}</v-list-item-subtitle>
+                      <v-list-item-subtitle v-if="o.iscoinbase" :data="o.iscoinbase">Coinbase:
+                        {{ o.iscoinbase }}
+                      </v-list-item-subtitle>
+                      <v-list-item-subtitle>Output Transaction:
+                        <router-link :to="o.output_transaction">
+                          {{ shortenHash(o.output_transaction) }}
+                        </router-link>
+                      </v-list-item-subtitle>
+                      <v-list-item-subtitle v-if="o.input_transaction">Input transaction:
+                        <router-link :to="o.input_transaction">
+                          {{ shortenHash(o.input_transaction) }}
+                        </router-link>
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import {shortenHash} from "@/utilities";
+
 export default {
   name: 'AddressLookup',
+  methods: {
+    shortenHash,
+  },
   computed: {
     data() {
       return this.$store.getters.getAddressData;
