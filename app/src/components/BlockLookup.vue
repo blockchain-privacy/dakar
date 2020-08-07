56d2be3532bad0dcea0ae6c7c6e53070eeeab711
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-08-07 14:10:22 +0200

    redesign of block view

--- app/src/components/BlockLookup.vue
+++ app/src/components/BlockLookup.vue
@@ -1,88 +1,119 @@
 <template>
-  <v-layout row v-if="data">
-    <v-flex xs12 sm8 offset-sm2>
-      <v-card>
-        <v-card-title>
-          <v-icon>mdi-bank-transfer</v-icon>
-          Block
-        </v-card-title>
-        <v-list two-line subheader>
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-icon>mdi-format-header-pound</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>Hash</v-list-item-title>
-              <v-list-item-subtitle>
-                {{ data.blockhash }}
-              </v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-avatar v-if="data.ts">
-              <v-icon>mdi mdi-calendar</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content v-if="data.ts">
-              <v-list-item-title>Timestamp</v-list-item-title>
-              <v-list-item-subtitle>
-                {{ data.ts != null ? new Date(data.ts).toLocaleString() : "" }}
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item v-if="data.id">
-            <v-list-item-avatar>
-              <v-icon>mdi-format-list-numbered</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>Block Height</v-list-item-title>
-              <v-list-item-subtitle>
-                {{ data.id }}
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-avatar v-if="data.prevblockhash">
-              <v-icon>mdi-format-header-pound</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content v-if="data.prevblockhash">
-              <v-list-item-title>Previous Block</v-list-item-title>
-              <v-list-item-subtitle>
-                <router-link :to="data.prevblockhash">{{ data.prevblockhash }}</router-link>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-avatar v-if="data.nextblockhash">
-              <v-icon>mdi-format-header-pound</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content v-if="data.nextblockhash">
-              <v-list-item-title>Next Block</v-list-item-title>
-              <v-list-item-subtitle>
-                <router-link :to="data.nextblockhash">{{ data.nextblockhash }}</router-link>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item v-for="tx in data.txhashes" v-bind:key="tx">
-            <v-list-item-avatar>
-              <v-icon></v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>Transaction</v-list-item-title>
-              <v-list-item-subtitle>
-                Hash:
-                <router-link :to="tx">{{ tx }}</router-link>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
-      <v-card><!-- TODO hack, to leave space at the bottom, such that content is not hidden by the footer.-->
-        <div style="height: 40pt">
-        </div>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container class="fill-height" fluid v-if="data">
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="12" md="10" lg="9" xl="8">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>
+              <v-icon>mdi-file-tree </v-icon>
+              Block
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-list-item>
+                    <v-list-item-avatar>
+                      <v-icon>mdi-format-header-pound</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                      <v-list-item-title>Hash</v-list-item-title>
+                      <v-list-item-subtitle>
+                        {{ shortenHash(data.blockhash) }}
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+                <v-col v-if="data.ts">
+                  <v-list-item>
+                    <v-list-item-avatar>
+                      <v-icon>mdi mdi-calendar</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                      <v-list-item-title>Timestamp</v-list-item-title>
+                      <v-list-item-subtitle>
+                        {{ data.ts != null ? new Date(data.ts).toLocaleString() : "" }}
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+              </v-row>
+              <v-row v-if="data.id">
+                <v-col>
+                  <v-list-item>
+                    <v-list-item-avatar>
+                      <v-icon>mdi-format-list-numbered</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                      <v-list-item-title>Block Height</v-list-item-title>
+                      <v-list-item-subtitle>
+                        {{ data.id }}
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col v-if="data.prevblockhash">
+                  <v-list-item>
+                    <v-list-item-avatar >
+                      <v-icon>mdi-format-header-pound</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content >
+                      <v-list-item-title>Previous Block</v-list-item-title>
+                      <v-list-item-subtitle>
+                        <router-link :to="data.prevblockhash">{{ shortenHash(data.prevblockhash) }}</router-link>
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+                <v-col>
+                  <v-list-item>
+                    <v-list-item-avatar v-if="data.nextblockhash">
+                      <v-icon>mdi-format-header-pound</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content v-if="data.nextblockhash">
+                      <v-list-item-title>Next Block</v-list-item-title>
+                      <v-list-item-subtitle>
+                        <router-link :to="data.nextblockhash">{{ shortenHash(data.nextblockhash) }}</router-link>
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+              </v-row>
+              <v-row v-if="data.txhashes">
+                <v-col v-for="tx in data.txhashes" v-bind:key="tx">
+                  <v-list-item>
+                    <v-list-item-avatar>
+                      <v-icon> mdi-transfer  </v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                      <v-list-item-title>Transaction</v-list-item-title>
+                      <v-list-item-subtitle>
+                        Hash:
+                        <router-link :to="tx">{{ shortenHash(tx) }}</router-link>
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import {shortenHash} from "@/utilities";
+
 export default {
   name: 'BlockLookup',
+  methods: {
+    shortenHash,
+  },
   computed: {
     data() {
       return this.$store.getters.getBlockData;
