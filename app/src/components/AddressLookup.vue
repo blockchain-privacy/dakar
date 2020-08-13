c40f4c02b3f054dbd3235037a2e994700da1bd85
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-08-13 14:50:54 +0200

    display balance, amount spent and amount received on address page

--- app/src/components/AddressLookup.vue
+++ app/src/components/AddressLookup.vue
@@ -12,19 +12,39 @@
           <v-card-text>
             <v-container>
               <v-row>
+                <v-col>
+                  <IconItem icon="mdi-scale-balance" title="Balance">
+                    {{ this.amounts.received - this.amounts.spent }}
+                  </IconItem>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <IconItem icon="mdi-bank-transfer-in" title="Total amount received">
+                    {{ this.amounts.received }}
+                  </IconItem>
+                </v-col>
+                <v-col>
+                  <IconItem icon="mdi-bank-transfer-out" title="Total amount spent">
+                    {{ this.amounts.spent }}
+                  </IconItem>
+                </v-col>
+              </v-row>
+              <v-divider></v-divider>
+              <v-row>
                 <v-col v-for="o in data.addr_outputs" v-bind:key="o.input_transaction + o.output_transaction">
                   <v-sheet min-height="50" class="fill-height" color="transparent">
                     <v-lazy min-height="90" transition="fade-transition" :options="{threshold: 1}">
                       <IconItem icon="mdi-currency-usd-circle-outline" title="Output">
                         Amount: {{ o.amount }}
-                        <br/>
+                        <br v-if="o.iscoinbase"/>
                         {{ o.iscoinbase ? 'Coinbase: ' + o.iscoinbase : '' }}
                         <br/>
                         Output Transaction:
                         <router-link :to="o.output_transaction">
                           {{ shortenHash(o.output_transaction) }}
                         </router-link>
-                        <br/>
+                        <br v-if="o.input_transaction"/>
                         {{ o.input_transaction ? 'Input transaction:' : '' }}
                         <router-link :to="o.input_transaction" v-if="o.input_transaction">
                           {{ shortenHash(o.input_transaction) }}
@@ -52,10 +72,27 @@ export default {
   components: {IconItem},
   methods: {
     shortenHash,
+    calculateAmountReceived: function (outputs) {
+      return outputs
+          .map(e => parseFloat(e.amount))
+          .reduce((sum, e) => sum + e, 0);
+    },
+    calculateAmountSpent: function (outputs) {
+      return outputs
+          .filter(e => e.input_transaction !== '')
+          .map(e => parseFloat(e.amount))
+          .reduce((sum, e) => sum + e, 0);
+    }
   },
   computed: {
     data() {
       return this.$store.getters.getAddressData;
+    },
+    amounts() {
+      return {
+        received: this.calculateAmountReceived(this.data.addr_outputs),
+        spent: this.calculateAmountSpent(this.data.addr_outputs),
+      };
     }
   },
   mounted() {
