4bf6f07779f533754a4357f8f2779b001f5b69ec
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-08-31 14:20:01 +0200

    remove dead code

--- dashrpc/cmd/query/main.go
+++ dashrpc/cmd/query/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	cli "dashrpc/cmd/cliutil"
-	"dashrpc/db"
-	"dashrpc/db/analytics"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"os"
 )
 
 const privateSendFilename = "result.csv"
@@ -48,58 +45,10 @@ func main() {
 		}()
 	}
 
-	dgraph, c, err := db.CreateDefaultClient()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	defer func() {
-		if err = c.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
-
 	if len(cliArgs.TxSearch) > 0 {
 
-		origins, err := analytics.AnalyzeOrigins(dgraph, cliArgs.TxSearch, uint(16))
-		if err != nil {
-			log.Println(err)
-			return
-		}
+		log.Println("Information is already available in the crawler tool. Query the database or use the frontend to find origins.")
 
-		for _, o := range origins {
-			fmt.Println(o)
-		}
-
-		os.Exit(0)
-		//err, res := transactionSearch(dgraph, cliArgs.TxSearch, "./"+privateSendFilename)
-		//
-		//if err != nil {
-		//	log.Println(err)
-		//	return
-		//}
-		//log.Println("Final map has", len(res), "elements")
-		//} else if len(cliArgs.TxInfo) > 0 {
-		//
-		//	transaction, err := dbtx.GetTransaction(dgraph, cliArgs.TxInfo)
-		//	if err != nil {
-		//		log.Fatal(err)
-		//	}
-		//
-		//	log.Println("Tx IsPrivacyOrigin:", transaction.IsPrivacyOrigin())
-		//	if transaction.IsPrivacyOrigin() {
-		//		log.Println("Denominations:", transaction.CountOutputDenominations())
-		//	}
-		//	log.Println("Tx isMixingTransaction:", transaction.IsMixing())
-		//	if transaction.IsMixing() {
-		//		log.Println("Denominations on outputs:", transaction.CountOutputDenominations())
-		//		log.Println("Denominations on inputs:", transaction.CountInputDenominations())
-		//	}
-		//	log.Println("Tx IsPrivacyDestination:", transaction.IsPrivacyDestination())
-		//	if transaction.IsPrivacyDestination() {
-		//		log.Println("Denominations on inputs:", transaction.CountInputDenominations())
-		//	}
 	} else if len(cliArgs.ClusterAddr) > 0 {
 		log.Println("Clustering is not yet implemented")
 	}
