71e5eb38c2d4ae5d2e3122802becccdbbd199a8a
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-09-29 14:26:21 +0200

    use dummy heuristic

--- dashrpc/cmd/query/main.go
+++ dashrpc/cmd/query/main.go
@@ -70,7 +70,9 @@ func main() {
 		//tx := "7336d112b9a2b838ea6fcedb0d55345308952f4dc67a8ff76ff3eba179ed31d4"
 		//other := "fdaad37eb8cd68291cc54089e63b75ad01df3c90d10fde26226875dbefc49cdb"
 
-		heuristic.DoHeuristic(dgraph, cliArgs.TxSearch)
+		if err := heuristic.DoHeuristic(dgraph, cliArgs.TxSearch, heuristic.NewDummyHeuristic()); err != nil {
+			return
+		}
 
 	} else if len(cliArgs.ClusterAddr) > 0 {
 		log.Println("Clustering is not yet implemented")
