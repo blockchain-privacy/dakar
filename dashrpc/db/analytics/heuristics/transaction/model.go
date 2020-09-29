2812691d8659493562cfe095eaa63bdcb7f1addc
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-09-29 14:25:32 +0200

    support adding heuristics to database

---
+++ dashrpc/db/analytics/heuristics/transaction/model.go
@@ -0,0 +1,21 @@
+package transaction
+
+const DType = "TransactionHeuristic"
+
+type DummyOrigin struct {
+	Uid string `json:"uid,omitempty"`
+}
+
+type Heuristic struct {
+	Uid           string        `json:"uid,omitempty"`
+	HeuristicType string        `json:"type,omitempty"`
+	TxUid         string        `json:"h_transaction,omitempty"`
+	Origins       []DummyOrigin `json:"results,omitempty"`
+	DType         []string      `json:"dgraph.type,omitempty"`
+	// only included for finding the tx uid in the upsert step
+	TxHash string `json:"-"`
+}
+
+func (h *Heuristic) SetDType() {
+	h.DType = []string{DType}
+}
