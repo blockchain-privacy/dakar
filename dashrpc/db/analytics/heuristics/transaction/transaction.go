fa3b70593712ef89665757a2498ae2e20d614cd7
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-09-30 10:19:49 +0200

    delete heuristic edges before inserting new information

--- dashrpc/db/analytics/heuristics/transaction/transaction.go
+++ dashrpc/db/analytics/heuristics/transaction/transaction.go
@@ -9,7 +9,14 @@ import (
 	"github.com/dgraph-io/dgo/v2/protos/api"
 )
 
-func UpsertHeuristic(c *dgo.Dgraph, h Heuristic) error {
+// Upserts the given heuristic
+func UpsertHeuristic(c *dgo.Dgraph, h Heuristic) (err error) {
+	// delete potential already existing edges of h
+	if err = deleteHeuristicEdges(c, h); err != nil {
+		err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
+		return
+	}
+
 	h.SetDType()
 	h.Uid = "uid(h)"
 	h.TxUid = "uid(tx)"
@@ -17,7 +24,7 @@ func UpsertHeuristic(c *dgo.Dgraph, h Heuristic) error {
 	pb, err := json.Marshal(h)
 	if err != nil {
 		err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
-		return err
+		return
 	}
 
 	query := `
@@ -40,7 +47,38 @@ func UpsertHeuristic(c *dgo.Dgraph, h Heuristic) error {
 
 	if err = db.TxWithRetry(c, db.GetBackendContext(), req); err != nil {
 		err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
+		return
 	}
 
-	return err
+	return
+}
+
+// Deletes all edges of the result predicate of h
+func deleteHeuristicEdges(c *dgo.Dgraph, h Heuristic) (err error) {
+	h.SetDType()
+	h.Uid = "uid(h)"
+
+	query := `
+		query Q($txhash: string, $type: string) {
+			var(func: eq(txhash, $txhash)){
+				h as ~h_transaction@filter(eq(type,$type))
+			}
+		}
+	`
+
+	mu := &api.Mutation{}
+	dgo.DeleteEdges(mu, "uid(h)", "results")
+
+	req := &api.Request{
+		Query:     query,
+		Vars:      map[string]string{"$txhash": h.TxHash, "$type": h.HeuristicType},
+		Mutations: []*api.Mutation{mu},
+		CommitNow: true,
+	}
+
+	if err = db.TxWithRetry(c, db.GetBackendContext(), req); err != nil {
+		err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
+	}
+
+	return
 }
