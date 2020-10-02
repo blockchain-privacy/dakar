01bb20d8b3894ced4993ace155e06c6d4178eba2
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-10-02 17:30:11 +0200

    add skeleton for testing an alternative analysation function

--- dashrpc/cmd/query/main.go
+++ dashrpc/cmd/query/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	heuristic "dashrpc/analytics/heuristics/transaction"
 	cli "dashrpc/cmd/cliutil"
 	"dashrpc/db"
+	dban "dashrpc/db/analytics"
 	"errors"
 	"flag"
 	"fmt"
@@ -43,12 +43,14 @@ func main() {
 	}
 
 	// setup Logging
-	if f, err := cli.GetLogfile(cliArgs.Logfile); err != nil {
-		defer func() {
-			if err = f.Close(); err != nil {
-				fmt.Println(err)
-			}
-		}()
+	if len(cliArgs.Logfile) > 0 {
+		if f, err := cli.GetLogfile(cliArgs.Logfile); err != nil {
+			defer func() {
+				if err = f.Close(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+		}
 	}
 
 	// create dgraph client
@@ -70,9 +72,18 @@ func main() {
 		//tx := "7336d112b9a2b838ea6fcedb0d55345308952f4dc67a8ff76ff3eba179ed31d4"
 		//other := "fdaad37eb8cd68291cc54089e63b75ad01df3c90d10fde26226875dbefc49cdb"
 
-		if err := heuristic.Exec(dgraph, cliArgs.TxSearch, heuristic.NewDummyHeuristic()); err != nil {
+		// heuristic test
+		//if err := heuristic.Exec(dgraph, cliArgs.TxSearch, heuristic.NewDummyHeuristic()); err != nil {
+		//	return
+		//}
+
+		// origin test
+		origins, err := dban.AnalyzeOriginsAlt(dgraph, cliArgs.TxSearch)
+		if err != nil {
 			return
 		}
+
+		log.Println(len(origins))
 
 	} else if len(cliArgs.ClusterAddr) > 0 {
 		log.Println("Clustering is not yet implemented")
