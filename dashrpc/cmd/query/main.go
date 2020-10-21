54e4070ba3cb37a8938888d954226e8be979c83f
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-10-22 00:44:02 +0200

    add test transaction

--- dashrpc/cmd/query/main.go
+++ dashrpc/cmd/query/main.go
@@ -71,12 +71,12 @@ func main() {
 		//biggerTransaction := "d3efe170dc1c1e8db2e8feb6fab76da2d9188176196d69b0055ca8e485233fd3"
 		//tx := "7336d112b9a2b838ea6fcedb0d55345308952f4dc67a8ff76ff3eba179ed31d4"
 		//other := "fdaad37eb8cd68291cc54089e63b75ad01df3c90d10fde26226875dbefc49cdb"
-
-		testTransaction2 := "78d7d55ecd30c78ea91bffdff536e9c4476d44aa1e2d874663cfba3a547a0eef"
+		testOneSource := "cfb95252da737464c9b37fcd294e1b19b3903b84ae7dd7a045bcb9765c0fb570\n"
+		//testTransaction2 := "78d7d55ecd30c78ea91bffdff536e9c4476d44aa1e2d874663cfba3a547a0eef"
 		//testTransation := "cc48f524a5201715428d25dc79a362a5a0fb21747370f224ca5cd2dc1e616862"
 
 		// heuristic test
-		if err := heuristic.Exec(dgraph, testTransaction2, heuristic.NewTimeConstraintHeuristic()); err != nil {
+		if err := heuristic.Exec(dgraph, testOneSource, heuristic.NewTimeConstraintHeuristic()); err != nil {
 			log.Println(err)
 			return
 		}
