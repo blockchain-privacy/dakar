d975f1184e7c9958a2b467d941cbd2dd02790923
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-11-27 12:47:13 +0100

    build heuristic executors

--- dashrpc/analytics/heuristics/transaction/queue.go
+++ dashrpc/analytics/heuristics/transaction/queue.go
@@ -6,15 +6,29 @@ import (
 	"errors"
 	"fmt"
 	"github.com/dgraph-io/dgo/v2"
+	"log"
 )
 
-var (
-	errHeuristicTypeNotFound = errors.New("error heuristic type not found")
-	errHeuristicNotValid     = errors.New("heuristics are not valid")
-)
-
+// validHeuristics includes all heuristics which are possible to receive from the frontend.
+// New heuristics must be added here
 var validHeuristics = []heuristic{NewOneSourceHeuristic(0), NewAmountHeuristic(),
 	NewPerfectMatchHeuristic(), NewDenominationTypeHeuristic()}
+
+// errors for this file
+var (
+	errHeuristicTypeNotFound  = errors.New("error heuristic type not found")
+	errHeuristicDuplicateUid  = errors.New("error duplicate uids found")
+	errHeuristicUidNotFound   = errors.New("error uid not found")
+	errHeuristicMultipleRoots = errors.New("error multiple roots found")
+	errHeuristicNotValid      = errors.New("error heuristics are not valid")
+)
+
+type heuristicTreeElement struct {
+	uid                string
+	parentHeuristicUid string
+	heuristic          heuristic
+	childHeuristicUid  []string
+}
 
 // isValid checks if the given heuristics are all valid
 func isValid(hMap map[string]heuristic, heuristics []dbtxh.FrontendHeuristic) bool {
@@ -28,11 +42,6 @@ func isValid(hMap map[string]heuristic, heuristics []dbtxh.FrontendHeuristic) bo
 			return false
 		}
 
-		// at least one must be set
-		if len(h.ParentHeuristic) == 0 && len(h.ChildHeuristics) == 0 {
-			return false
-		}
-
 		// type must by in valid set; parameter must be set if the map has a parameter
 		if heuristic, ok := hMap[h.Type]; !ok || (heuristic.hasParameter() && len(h.Parameter) == 0) {
 			return false
@@ -42,10 +51,20 @@ func isValid(hMap map[string]heuristic, heuristics []dbtxh.FrontendHeuristic) bo
 	return true
 }
 
-func buildHeuristics(hMap map[string]heuristic, heuristics []dbtxh.FrontendHeuristic) (builtHeuristics []heuristic,
+func buildHeuristicTreeElements(hMap map[string]heuristic, heuristics []dbtxh.FrontendHeuristic) (builtHeuristics map[string]heuristicTreeElement,
 	err error) {
+	// create map
+	builtHeuristics = make(map[string]heuristicTreeElement)
+
+	// add elements to map
 	for _, h := range heuristics {
+		// create new heuristic
 		if newHeuristic, ok := hMap[h.Type]; ok {
+			if _, ok := builtHeuristics[h.Uid]; ok {
+				err = errHeuristicDuplicateUid
+				return
+			}
+
 			if newHeuristic.hasParameter() {
 				err = newHeuristic.setParameter(h.Parameter)
 				if err != nil {
@@ -54,11 +73,160 @@ func buildHeuristics(hMap map[string]heuristic, heuristics []dbtxh.FrontendHeuri
 				}
 			}
 
-			builtHeuristics = append(builtHeuristics, newHeuristic)
+			var childHeuristicUids []string
+
+			for _, c := range h.ChildHeuristics {
+				childHeuristicUids = append(childHeuristicUids, c.Uid)
+			}
+
+			var parentUid string
+
+			if len(h.ParentHeuristic) > 0 {
+				parentUid = h.ParentHeuristic[0].Uid
+			}
+
+			builtHeuristics[h.Uid] = heuristicTreeElement{
+				uid:                h.Uid,
+				parentHeuristicUid: parentUid,
+				heuristic:          newHeuristic,
+				childHeuristicUid:  childHeuristicUids,
+			}
 
 		} else {
 			err = errHeuristicTypeNotFound
 			return
+		}
+	}
+
+	return
+}
+
+type treeLeaf struct {
+	uid   string
+	level int
+}
+
+// traverseHeuristicTree is a recursive function called by getNodeLevelDistribution. It traverses a tree starting
+// at root and returns the node level of itself and all child nodes
+func traverseHeuristicTree(nodes map[string]heuristicTreeElement, rootUid string, root heuristicTreeElement,
+	level int) (l []treeLeaf, err error) {
+	for _, uid := range root.childHeuristicUid {
+		if h, ok := nodes[uid]; ok {
+
+			leafs, traverseErr := traverseHeuristicTree(nodes, uid, h, level+1)
+			if traverseErr != nil {
+				err = traverseErr
+				return
+			}
+
+			l = append(l, leafs...)
+		} else {
+			err = errHeuristicUidNotFound
+			return
+		}
+	}
+
+	l = append(l, treeLeaf{
+		uid:   rootUid,
+		level: level,
+	})
+
+	return
+}
+
+// getNodeLevelDistribution returns a list of all nodes with the respective level in the tree
+func getNodeLevelDistribution(nodes map[string]heuristicTreeElement, rootUid string,
+	root heuristicTreeElement) (levelToNode [][]heuristicTreeElement, err error) {
+	treeNodes, err := traverseHeuristicTree(nodes, rootUid, root, 0)
+	if err != nil {
+		err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
+		return
+	}
+
+	for _, n := range treeNodes {
+		level := n.level
+		twoDimLength := len(levelToNode)
+		// add elements to first dimension of array if not exist
+		if twoDimLength < level+1 {
+			for i := 0; i < level+1-twoDimLength; i++ {
+				levelToNode = append(levelToNode, []heuristicTreeElement{})
+			}
+		}
+
+		levelToNode[level] = append(levelToNode[level], nodes[n.uid])
+	}
+
+	return
+}
+
+// buildExecutorsFromLevels builds a HeuristicExecutor beginning at the deepest level of the tree
+func buildExecutorsFromLevels(levelToNode [][]heuristicTreeElement) (rootExecutor HeuristicExecutor, err error) {
+	executorStack := make(map[string]HeuristicExecutor)
+
+	// backward traversal of list, because the deepest level has the highest index
+	for i := len(levelToNode) - 1; i >= 0; i-- {
+		for _, n := range levelToNode[i] {
+			thisExec := BuildExecutor(n.heuristic)
+
+			for _, childUid := range n.childHeuristicUid {
+				if child, ok := executorStack[childUid]; ok {
+					// add child to this executor tree and delete it from the map
+					thisExec.NextHeuristics = append(thisExec.NextHeuristics, child)
+					delete(executorStack, childUid)
+				} else {
+					err = errHeuristicUidNotFound
+					return
+				}
+			}
+
+			// add new executor to map
+			executorStack[n.uid] = thisExec
+		}
+	}
+
+	// remaining element in executorStack must be one -> only one root allowed
+	if len(executorStack) != 1 {
+		err = errHeuristicMultipleRoots
+		return
+	}
+
+	// set rootExecutor to the only remaining value
+	for _, v := range executorStack {
+		rootExecutor = v
+	}
+
+	return
+}
+
+func buildExecutors(heuristics map[string]heuristicTreeElement) (executors []HeuristicExecutor, err error) {
+	//processedElements := make(map[string]heuristicTreeElement)
+
+	var roots []heuristicTreeElement
+	for k, v := range heuristics {
+		// start with root nodes
+		if v.parentHeuristicUid == "" {
+			roots = append(roots, v)
+
+			if len(v.childHeuristicUid) == 0 {
+				executors = append(executors, HeuristicExecutor{
+					ThisHeuristic: v.heuristic,
+				})
+				// no children -> no work
+				continue
+			}
+
+			levelDistribution, childErr := getNodeLevelDistribution(heuristics, k, v)
+			if childErr != nil {
+				err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), childErr)
+				return
+			}
+
+			if newRootExecutor, buildErr := buildExecutorsFromLevels(levelDistribution); buildErr != nil {
+				err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), buildErr)
+				return
+			} else {
+				executors = append(executors, newRootExecutor)
+			}
 		}
 	}
 
@@ -76,12 +244,17 @@ func DoExecution(dgraph *dgo.Dgraph, heuristics []dbtxh.FrontendHeuristic) error
 		return errHeuristicNotValid
 	}
 
-	newHeuristics, err := buildHeuristics(heuristicMap, heuristics)
+	newHeuristics, err := buildHeuristicTreeElements(heuristicMap, heuristics)
 	if err != nil {
 		return fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
 	}
 
-	fmt.Println(newHeuristics)
+	executors, err := buildExecutors(newHeuristics)
+	if err != nil {
+		return fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
+	}
+
+	log.Println(executors)
 
 	return nil
 }
