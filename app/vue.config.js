1750ebae2a5afce82fa94601229aaf561fb4105f
Michael Ziegler <michael.h.ziegler@ntnu.no>
2020-12-07 16:31:08 +0100

    comply with eslint

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   devServer: {
-    proxy: "http://localhost:8081",
+    proxy: 'http://localhost:8081',
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": 'GET, POST',
-      "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-    }
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+    },
   },
 
   transpileDependencies: [
-    "vuetify",
+    'vuetify',
   ],
-  runtimeCompiler: true
-}
+  runtimeCompiler: true,
+};
