f16beaaa8cd40968bd02eea9a30a6c4eeead4db3
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-01-15 09:56:28 +0100

    add user menu

--- app/src/App.vue
+++ app/src/App.vue
@@ -15,6 +15,36 @@
       <v-spacer></v-spacer>
       <QueryInput class="mx-4"/>
       <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on">
+            <v-icon>{{ icon.mdiAccount }}</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click="goToLogin">
+            <v-list-item-icon>
+              <v-icon>{{ icon.mdiLogin }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="goToUserAdministration">
+            <v-list-item-icon>
+              <v-icon>{{ icon.mdiAccountSupervisor }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>User Administration</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>{{ icon.mdiLogout }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-btn icon v-on:click="changeTheme()">
         <v-icon dark>{{ icon.mdiInvertColors }}</v-icon>
       </v-btn>
@@ -39,7 +69,7 @@
 
 <script>
 import {
-  mdiInvertColors,
+  mdiInvertColors, mdiAccount, mdiLogin, mdiLogout, mdiAccountSupervisor,
 } from '@mdi/js';
 import QueryInput from './components/QueryInput.vue';
 import MsgBox from './components/MsgBox.vue';
@@ -56,7 +86,7 @@ export default {
     return {
       applicationName: Constants.APPLICATION_NAME,
       icon: {
-        mdiInvertColors,
+        mdiInvertColors, mdiAccount, mdiLogin, mdiLogout, mdiAccountSupervisor,
       },
     };
   },
@@ -68,6 +98,16 @@ export default {
       // only change route if not already on entry page
       if (this.$route.name === Constants.ROUTE_NAME_ENTRY_PAGE) return;
       this.$router.push({ name: Constants.ROUTE_NAME_ENTRY_PAGE });
+    },
+    goToLogin() {
+      // only change route if not already on entry page
+      if (this.$route.name === Constants.ROUTE_NAME_LOGIN_PAGE) return;
+      this.$router.push({ name: Constants.ROUTE_NAME_LOGIN_PAGE });
+    },
+    goToUserAdministration() {
+      // only change route if not already on entry page
+      if (this.$route.name === Constants.ROUTE_NAME_USER_ADMIN_PAGE) return;
+      this.$router.push({ name: Constants.ROUTE_NAME_USER_ADMIN_PAGE });
     },
   },
   beforeMount() {
@@ -82,8 +122,7 @@ export default {
   transition: opacity 0.2s ease;
 }
 
-.component-fade-enter, .component-fade-leave-to
-{
+.component-fade-enter, .component-fade-leave-to {
   opacity: 0;
 }
 </style>
