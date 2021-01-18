1fadf6d386443deccaccaff75570bd023a49802e
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-01-18 10:04:21 +0100

    add user validation functions

--- backend/db/user/model.go
+++ backend/db/user/model.go
@@ -1,7 +1,9 @@
 package user
 
 import (
+	"backend/user"
 	"fmt"
+	"regexp"
 	"time"
 )
 
@@ -17,7 +19,7 @@ type Role struct {
 }
 
 func (r Role) String() string {
-	return fmt.Sprintf("uid: %s, name %s", r.Uid, r.Name)
+	return fmt.Sprintf("uid: %s, name: %s", r.Uid, r.Name)
 }
 
 func (r *Role) SetDType() {
@@ -25,12 +27,13 @@ func (r *Role) SetDType() {
 }
 
 type User struct {
-	Uid      string    `json:"uid,omitempty"`
-	Email    string    `json:"user_email"`
-	Roles    []Role    `json:"user_roles"`
-	Created  time.Time `json:"user_created"`
-	Modified time.Time `json:"user_modified"`
-	DType    []string  `json:"dgraph.type,omitempty"`
+	Uid          string    `json:"uid,omitempty"`
+	Email        string    `json:"user_email"`
+	PasswordHash string    `json:"user_pwhash"`
+	Roles        []Role    `json:"user_roles"`
+	Created      time.Time `json:"user_created"`
+	Modified     time.Time `json:"user_modified"`
+	DType        []string  `json:"dgraph.type,omitempty"`
 }
 
 func (u User) String() string {
@@ -68,4 +71,39 @@ func (f FrontendUserCreate) ToUser() User {
 		Email: f.Email,
 		Roles: roles,
 	}
+}
+
+// isValidEmail is a regex filter which checks if the input conforms to an email string
+var isValidEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]" +
+	"{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$").MatchString
+
+// IsValid does a sanity check for the given FrontendUserCreate
+func (f FrontendUserCreate) IsValid() bool {
+	// check if values are set
+	if len(f.Email) == 0 || len(f.Roles) == 0 || !isValidEmail(f.Email) {
+		return false
+	}
+
+	// check if all roles have valid values
+	for _, ur := range f.Roles {
+		if _, err := user.GetRoleByName(ur); err != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+type FrontendUserLogin struct {
+	Email    string `json:"user_email"`
+	Password string `json:"user_pw"`
+}
+
+func (f FrontendUserLogin) String() string {
+	return fmt.Sprintf("email %s, pw: %s", f.Email, f.Password)
+}
+
+// IsValid does a sanity check for the given FrontendUserLogin
+func (f FrontendUserLogin) IsValid() bool {
+	return len(f.Email) > 0 || len(f.Password) > 0
 }
