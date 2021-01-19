9a1d9b7dd382d9212566dabeb86297a2fe74af99
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-01-19 23:32:00 +0100

    check role in authorization middleware

--- backend/server/middleware.go
+++ backend/server/middleware.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"backend/cmd/cliutil"
+	"backend/user"
 	"encoding/json"
 	"net/http"
 	"time"
@@ -24,7 +25,7 @@ func writeUnauthorized(w http.ResponseWriter) {
 	}
 }
 
-func authorizationMiddleware() Adapter {
+func authorizationMiddleware(route string) Adapter {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			serverInfo(r.Cookies())
@@ -47,20 +48,44 @@ func authorizationMiddleware() Adapter {
 					return
 				}
 
+				userFromToken := token.Get(tokenFieldUser)
+				if len(userFromToken) == 0 {
+					writeUnauthorized(w)
+					serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
+					return
+				}
+
+				var newUser tokenUser
+				if jsonErr := json.Unmarshal([]byte(userFromToken), &newUser); jsonErr != nil {
+					serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
+					return
+				}
+
+				// check if route is allowed
+				routeAllowed := false
+				for _, uRole := range newUser.Roles {
+					routeRole, roleErr := user.GetRoleByName(uRole.Name)
+					if roleErr != nil {
+						writeUnauthorized(w)
+						serverInfo(cliutil.ShowCallInfo(), roleErr)
+						return
+					}
+
+					if routeRole.IsRouteAllowed(route) {
+						routeAllowed = true
+						break
+					}
+				}
+
+				if !routeAllowed {
+					writeUnauthorized(w)
+					serverInfo(cliutil.ShowCallInfo(), newUser.Id, "tried to access", route)
+					return
+				}
+
 				if timeUntilTokenExpires <= reissueDuration {
-					userFromToken := token.Get(tokenFieldUser)
-					if len(userFromToken) == 0 {
-						serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
-						return
-					}
-
-					var newUser tokenUser
-					if jsonErr := json.Unmarshal([]byte(userFromToken), &newUser); jsonErr != nil {
-						serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
-						return
-					}
-
 					if tokenErr := writeNewToken(w, newUser.toUser()); tokenErr != nil {
+						writeUnauthorized(w)
 						serverInfo(cliutil.ShowCallInfo(), tokenErr)
 						return
 					}
