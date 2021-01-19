a069a639ea7d9fe69e6919c533a254f2e63c903a
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-01-20 00:58:54 +0100

    send invalid token message

--- backend/server/middleware.go
+++ backend/server/middleware.go
@@ -25,74 +25,81 @@ func writeUnauthorized(w http.ResponseWriter) {
 	}
 }
 
+// sendRedirectMessage sends a redirect message
+func sendRedirectMessage(w http.ResponseWriter) {
+	setDefaultHeader(w)
+	if _, err := w.Write([]byte(`{"invalidToken": true}`)); err != nil {
+		http.Error(w, "encoding error", http.StatusInternalServerError)
+		serverInfo(cliutil.ShowCallInfo(), err)
+	}
+}
+
 func authorizationMiddleware(route string) Adapter {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			serverInfo(r.Cookies())
-
-			if cookie, err := r.Cookie(cookieTokenName); err != nil {
-				writeUnauthorized(w)
+			cookie, err := r.Cookie(cookieTokenName)
+			if err != nil {
+				sendRedirectMessage(w)
 				return
-			} else {
-				token, _, verifyErr := verifyToken(cookie.Value)
-				if verifyErr != nil {
-					writeUnauthorized(w)
-					return
-				}
-
-				timeUntilTokenExpires := time.Until(token.Expiration)
-				// todo instead of returning an error message, return a status that the user should
-				// be redirected to the login page
-				if timeUntilTokenExpires <= 0 {
-					writeUnauthorized(w)
-					return
-				}
-
-				userFromToken := token.Get(tokenFieldUser)
-				if len(userFromToken) == 0 {
-					writeUnauthorized(w)
-					serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
-					return
-				}
-
-				var newUser tokenUser
-				if jsonErr := json.Unmarshal([]byte(userFromToken), &newUser); jsonErr != nil {
-					serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
-					return
-				}
-
-				// check if route is allowed
-				routeAllowed := false
-				for _, uRole := range newUser.Roles {
-					routeRole, roleErr := user.GetRoleByName(uRole.Name)
-					if roleErr != nil {
-						writeUnauthorized(w)
-						serverInfo(cliutil.ShowCallInfo(), roleErr)
-						return
-					}
-
-					if routeRole.IsRouteAllowed(route) {
-						routeAllowed = true
-						break
-					}
-				}
-
-				if !routeAllowed {
-					writeUnauthorized(w)
-					serverInfo(cliutil.ShowCallInfo(), newUser.Id, "tried to access", route)
-					return
-				}
-
-				if timeUntilTokenExpires <= reissueDuration {
-					if tokenErr := writeNewToken(w, newUser.toUser().ToFrontendUserState()); tokenErr != nil {
-						writeUnauthorized(w)
-						serverInfo(cliutil.ShowCallInfo(), tokenErr)
-						return
-					}
-				}
-
-				h.ServeHTTP(w, r)
 			}
+
+			token, _, verifyErr := verifyToken(cookie.Value)
+			if verifyErr != nil {
+				sendRedirectMessage(w)
+				return
+			}
+
+			timeUntilTokenExpires := time.Until(token.Expiration)
+			if timeUntilTokenExpires <= 0 {
+				sendRedirectMessage(w)
+				return
+			}
+
+			userFromToken := token.Get(tokenFieldUser)
+			if len(userFromToken) == 0 {
+				sendRedirectMessage(w)
+				serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
+				return
+			}
+
+			var newUser tokenUser
+			if jsonErr := json.Unmarshal([]byte(userFromToken), &newUser); jsonErr != nil {
+				writeUnauthorized(w)
+				serverInfo(cliutil.ShowCallInfo(), "user id field not found in token")
+				return
+			}
+
+			// check if route is allowed
+			routeAllowed := false
+			for _, uRole := range newUser.Roles {
+				routeRole, roleErr := user.GetRoleByName(uRole.Name)
+				if roleErr != nil {
+					writeUnauthorized(w)
+					serverInfo(cliutil.ShowCallInfo(), roleErr)
+					return
+				}
+
+				if routeRole.IsRouteAllowed(route) {
+					routeAllowed = true
+					break
+				}
+			}
+
+			if !routeAllowed {
+				writeUnauthorized(w)
+				serverInfo(cliutil.ShowCallInfo(), newUser.Id, "tried to access", route)
+				return
+			}
+
+			if timeUntilTokenExpires <= reissueDuration {
+				if tokenErr := writeNewToken(w, newUser.toUser().ToFrontendUserState()); tokenErr != nil {
+					sendRedirectMessage(w)
+					serverInfo(cliutil.ShowCallInfo(), tokenErr)
+					return
+				}
+			}
+
+			h.ServeHTTP(w, r)
 		})
 	}
 }
