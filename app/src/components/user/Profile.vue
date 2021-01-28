4c2d3285e65d0a8a0b4ec7b5f9a66874110a5fea
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-01-28 15:03:36 +0100

    add password and email change dialogs

--- app/src/components/user/Profile.vue
+++ app/src/components/user/Profile.vue
@@ -14,6 +14,70 @@
                  :icon="item.icon"
                  :item-value="item.val"
                  :action-function="item.actionFunction"/>
+    <v-dialog v-model="showModifyEmailDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Change E-mail</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-form ref="modifyEmailForm">
+                <v-text-field
+                    v-model="editedEmailItem.email"
+                    label="E-mail"
+                    type="email"
+                    :rules="rules.emailRules">
+                </v-text-field>
+                <v-text-field
+                    v-model="editedEmailItem.current_password"
+                    label="Current password"
+                    type="password"
+                    :rules="rules.passwordRules">
+                </v-text-field>
+              </v-form>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeEmailForm">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="saveEmailForm">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="showModifyPasswordDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Change Password</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-form ref="modifyPasswordForm">
+                <v-text-field
+                    v-model="editedPasswordItem.new_password"
+                    label="New Password"
+                    type="password"
+                    :rules="rules.passwordRules">
+                </v-text-field>
+                <v-text-field
+                    v-model="editedEmailItem.current_password"
+                    label="Current password"
+                    type="password"
+                    :rules="rules.passwordRules">
+                </v-text-field>
+              </v-form>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closePasswordForm">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="savePasswordForm">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -21,9 +85,13 @@
 import {
   mdiLock, mdiEmail, mdiCalendar, mdiCalendarEdit, mdiAccountDetails,
 } from '@mdi/js';
-import { LOCALSTORAGE_FIELD_USER, PAGE_TITLE, ROUTE_USER_MODIFY } from '../../constants';
+import {
+  LOCALSTORAGE_FIELD_USER, PAGE_TITLE, ROUTE_USER_MODIFY,
+} from '../../constants';
 import ProfileItem from './ProfileItem.vue';
-import { doPost, handleError } from '../../utilities';
+import {
+  doPost, emailRules, handleError, passwordRules,
+} from '../../utilities';
 
 export default {
   name: 'Profile',
@@ -32,6 +100,27 @@ export default {
     return {
       icon: {
         mdiLock, mdiEmail, mdiCalendar, mdiCalendarEdit, mdiAccountDetails,
+      },
+      showModifyEmailDialog: false,
+      showModifyPasswordDialog: false,
+      rules: {
+        emailRules, passwordRules,
+      },
+      editedEmailItem: {
+        email: '',
+        current_password: '',
+      },
+      defaultEmailItem: {
+        email: '',
+        current_password: '',
+      },
+      editedPasswordItem: {
+        new_password: '',
+        current_password: '',
+      },
+      defaultPasswordItem: {
+        new_password: '',
+        current_password: '',
       },
     };
   },
@@ -56,13 +145,13 @@ export default {
           title: 'Email:',
           val: this.userData.email,
           icon: this.icon.mdiEmail,
-          actionFunction: this.dummyFunc,
+          actionFunction: this.openEmailForm,
         },
         {
           title: 'Change Password:',
           val: '********',
           icon: this.icon.mdiLock,
-          actionFunction: this.dummyFunc,
+          actionFunction: this.openPasswordForm,
         },
         {
           title: 'Account last modified:',
@@ -78,16 +167,72 @@ export default {
     },
   },
   methods: {
-    dummyFunc() {
+    /** Email methods * */
+    closeEmailForm() {
+      this.showModifyEmailDialog = false;
+      this.$nextTick(() => {
+        this.editedEmailItem = { ...this.defaultEmailItem };
+      });
+    },
+    openEmailForm() {
+      this.showModifyEmailDialog = true;
+    },
+    validateEmailForm() {
+      return this.$refs.modifyEmailForm.validate();
+    },
+    saveEmailForm() {
+      if (!this.validateEmailForm()) return;
+
       doPost(ROUTE_USER_MODIFY, this.$router, {
         uid: this.userData.uid,
-        email: this.userData.email,
+        email: this.editedEmailItem.email,
+        current_password: this.editedEmailItem.current_password,
       })
         .then((data) => {
-          if (data) localStorage.setItem(LOCALSTORAGE_FIELD_USER, JSON.stringify(data));
+          if (data.user) {
+            localStorage.setItem(LOCALSTORAGE_FIELD_USER, JSON.stringify(data.user));
+            this.userData = data.user;
+          }
         })
         .catch((e) => {
           handleError(this.$store, e);
+        })
+        .finally(() => {
+          this.closeEmailForm();
+        });
+    },
+    /** Password methods * */
+    closePasswordForm() {
+      this.showModifyPasswordDialog = false;
+      this.$nextTick(() => {
+        this.editedPasswordItem = { ...this.defaultPasswordItem };
+      });
+    },
+    openPasswordForm() {
+      this.showModifyPasswordDialog = true;
+    },
+    validatePasswordForm() {
+      return this.$refs.modifyPasswordForm.validate();
+    },
+    savePasswordForm() {
+      if (!this.validatePasswordForm()) return;
+
+      doPost(ROUTE_USER_MODIFY, this.$router, {
+        uid: this.userData.uid,
+        new_password: this.editedPasswordItem.new_password,
+        current_password: this.editedEmailItem.current_password,
+      })
+        .then((data) => {
+          if (data.user) {
+            localStorage.setItem(LOCALSTORAGE_FIELD_USER, JSON.stringify(data.user));
+            this.userData = data.user;
+          }
+        })
+        .catch((e) => {
+          handleError(this.$store, e);
+        })
+        .finally(() => {
+          this.closePasswordForm();
         });
     },
   },
