c2cd8f7dae75c811a686c4cba1511e2dc60037fe
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-02-12 15:39:47 +0100

    add box shadow to message box

--- app/src/components/MsgBox.vue
+++ app/src/components/MsgBox.vue
@@ -2,23 +2,23 @@
   <v-layout>
     <div class="msgBox">
       <v-alert xs6 :value="infoMsg && infoMsg !== ''" type="info"
-               class="floating" max-width="400px" prominent outlined text
+               class="message" max-width="400px" prominent outlined text border="left"
                dismissible v-model="isInfoActive">
         {{ infoMsg }}
       </v-alert>
 
       <v-alert xs6 :value="errorMsg && errorMsg !== '' &&  isValidError(errorMsg)" type="error"
-               class="floating" max-width="400px" prominent outlined text border="left"
+               class="message" max-width="400px" prominent outlined text border="left"
                dismissible v-model="isErrorActive">
         {{ errorMsg }}
       </v-alert>
       <v-alert xs6 :value="successMsg && successMsg !== ''" type="success"
-               class="floating" max-width="400px" prominent outlined text border="left"
+               class="message" max-width="400px" prominent outlined text border="left"
                dismissible v-model="isSuccessActive">
         {{ successMsg }}
       </v-alert>
       <v-alert xs6 :value="warningMsg && warningMsg !== ''" type="warning"
-               class="floating" max-width="400px" prominent outlined text border="left"
+               class="message" max-width="400px" prominent outlined text border="left"
                dismissible v-model="isWarningActive">
         {{ warningMsg }}
       </v-alert>
@@ -92,8 +92,10 @@ export default {
   top: 5px;
 }
 
-.floating {
+.message {
   background-color: white !important;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+  0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 }
 
 /* >>> for deep selection, 0 height and width for removing
