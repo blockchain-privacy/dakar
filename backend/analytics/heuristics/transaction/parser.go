d1ceebf358303d90ee7b18863c7d65cc13201739
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-02-18 10:29:48 +0100

    actually compare uid and prefix

--- backend/analytics/heuristics/transaction/parser.go
+++ backend/analytics/heuristics/transaction/parser.go
@@ -220,7 +220,7 @@ func isRootHeuristic(h heuristicTreeElement, heuristics map[string]heuristicTree
 		return true
 	}
 
-	// parentuid does not exist in change set -> must be a root element in this context
+	// parent uid does not exist in change set -> must be a root element in this context
 	if _, ok := heuristics[h.parentHeuristicUid]; !ok {
 		return true
 	}
@@ -293,7 +293,7 @@ func ConstructExecutors(dgraph *dgo.Dgraph, txhash string, heuristics []dbtxh.Fr
 		if isRootHeuristic(v, newHeuristics) {
 			rootUids = append(rootUids, v.uid)
 			// no need to check roots which are newly created and thus have no valid uid
-			if !strings.HasPrefix(newUidPrefix, newUidPrefix) {
+			if !strings.HasPrefix(v.uid, newUidPrefix) {
 				rootsToCheck = append(rootsToCheck, v.uid)
 			}
 		}
