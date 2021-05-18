e466cab6a11a8b437461339f07979f21e68b9632
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-05-18 10:42:45 +0200

    add configuration option for in memory transaction graph

--- backend/analytics/config.go
+++ backend/analytics/config.go
@@ -11,32 +11,37 @@ type Config struct {
 	IsClassifyingEnabled bool
 	// ClassifierStartBlock is the block id after classifications starts.
 	ClassifierStartBlock uint64
+	// IsInMemoryTransactionGraphEnabled controls if transactions get loaded in memory
+	IsInMemoryTransactionGraphEnabled bool
 }
 
 func NewDashConfig() Config {
 	return Config{
 		BlockchainName:     "Dash",
 		IsAnalysingEnabled: true,
-		// after block height 323756 the first mixing transactions appear
-		// which have the most recent format (same number of inputs and outputs)
-		AnalyseStartBlock:    323756,
-		IsClassifyingEnabled: true,
-		ClassifierStartBlock: 323756,
+		// after block height 323756 the first mixing transactions with the
+		// most recent format (same number of inputs and outputs) appear
+		AnalyseStartBlock:                 323756,
+		IsClassifyingEnabled:              true,
+		ClassifierStartBlock:              323756,
+		IsInMemoryTransactionGraphEnabled: true,
 	}
 }
 
 func NewBitcoinConfig() Config {
 	return Config{
-		BlockchainName:       "Bitcoin",
-		IsAnalysingEnabled:   false,
-		IsClassifyingEnabled: false,
+		BlockchainName:                    "Bitcoin",
+		IsAnalysingEnabled:                false,
+		IsClassifyingEnabled:              false,
+		IsInMemoryTransactionGraphEnabled: false,
 	}
 }
 
 func NewDogecoinConfig() Config {
 	return Config{
-		BlockchainName:       "Doge",
-		IsAnalysingEnabled:   false,
-		IsClassifyingEnabled: false,
+		BlockchainName:                    "Doge",
+		IsAnalysingEnabled:                false,
+		IsClassifyingEnabled:              false,
+		IsInMemoryTransactionGraphEnabled: false,
 	}
 }
