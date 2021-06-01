88844877019ec313013ecec83a55014f8ef09c86
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-06-01 16:30:37 +0200

    localize number of results for connection lookup

--- app/src/components/tools/ConnectionLookup.vue
+++ app/src/components/tools/ConnectionLookup.vue
@@ -102,7 +102,9 @@
         </v-col>
       </v-row>
       <v-divider class="my-3" v-if="this.transactions.length > 0"/>
-      <p v-if="this.transactionCount >=0">Found {{ this.transactionCount }} transactions</p>
+      <p v-if="this.transactionCount >=0">
+        Found {{ this.transactionCount.toLocaleString() }} transactions
+      </p>
       <p v-if="this.transactions.length > 30">Transaction list is limited to 30 transactions.</p>
       <div v-if="this.transactions.length > 0">
         <v-card outlined v-for="(tx) in transactions" :key="tx.txhash" class="mt-2">
