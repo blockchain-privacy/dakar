3010afd63aa05035be28ad5c04103cf481e46809
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-06-03 14:46:03 +0200

    add hover effect for page tiles

--- app/src/components/common/LinkCard.vue
+++ app/src/components/common/LinkCard.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-card class="mx-auto px-2" tile flat :to="to" max-width="90">
-    <div class="d-flex justify-center">
-      <v-icon size="40">{{ icon }}</v-icon>
-    </div>
-    <div class="d-flex justify-center pt-1">
-      <div class="text-subtitle-1 title-text">{{ title }}</div>
-    </div>
-  </v-card>
+  <v-hover v-slot:default="{ hover }">
+    <v-card
+        :class="{'on-hover': hover, 'mx-auto px-2': true}"
+        rounded flat :to="to" max-width="90">
+      <div class="d-flex justify-center">
+        <v-icon size="37">{{ icon }}</v-icon>
+      </div>
+      <div class="d-flex justify-center pt-1">
+        <div class="text-subtitle-2 title-text">{{ title }}</div>
+      </div>
+    </v-card>
+  </v-hover>
+
 </template>
 
 <script>
@@ -21,8 +26,15 @@ export default {
 </script>
 
 <style scoped>
+
 .title-text {
   text-align: center;
   line-height: 1.3;
 }
+
+.on-hover::before {
+  opacity: 0.08;
+  border-radius: 4px;
+}
+
 </style>
