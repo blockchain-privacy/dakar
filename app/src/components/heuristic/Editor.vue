1d67cfc142a218c6c30848d3af278ccb31ebec3d
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-08-02 12:16:21 +0200

    refactor type selection into own component

--- app/src/components/heuristic/Editor.vue
+++ app/src/components/heuristic/Editor.vue
@@ -83,67 +83,12 @@
           </v-list-item>
         </v-list>
       </v-menu>
-      <v-bottom-sheet scrollable v-model="isAddHeuristicSheetOpen">
-        <v-card>
-          <div class="d-flex" style="align-items: center">
-            <div class="mr-auto ml-2 text-h6">
-              Add heuristic</div>
-            <v-switch
-                class="mr-2"
-                v-model="isHeuristicSheetFixed"
-                label="Fixed"
-            ></v-switch>
-          </div>
-          <v-card-text style="height: 80%">
-            <v-tabs v-model="heuristicTabs" v-if="heuristicTabItems">
-              <v-tab v-for="item in heuristicTabItems" :key="item">
-                {{ item }}
-              </v-tab>
-            </v-tabs>
-            <v-tabs-items v-model="heuristicTabs" class="mt-3">
-              <v-tab-item transition="fade-transition"
-                  v-for="item in heuristicTabItems"
-                  :key="item">
-                <div class="d-flex flex-wrap" style="align-items: flex-start;">
-                  <v-card
-                      outlined
-                      v-for="(item, index)
-                      in heuristicDescriptors.filter((e) => {
-                        if(!e.category && item === 'Other')
-                          return true;
-                        return e.category === item
-                      })"
-                      :key="index"
-                      class="mx-3 mb-6"
-                      max-width="300">
-                    <v-card-title>
-                      {{ item.title }}
-                    </v-card-title>
-                    <v-card-subtitle>
-                      {{ item.description }}
-                    </v-card-subtitle>
-                    <v-card-subtitle>
-                      <v-form v-model="item.parameter.valid" v-if="item.parameter !== undefined">
-                        <v-text-field
-                            v-model="item.parameter.value"
-                            :rules="parameterRules.get(item.parameter.type)"
-                            :label="item.parameter.description"
-                            required>
-                        </v-text-field>
-                      </v-form>
-                    </v-card-subtitle>
-                    <v-card-actions class="pt-0">
-                      <v-btn outlined color="primary" @click="addNewHeuristicAction(item)">
-                        Add Heuristic
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </div>
-              </v-tab-item>
-            </v-tabs-items>
-          </v-card-text>
-        </v-card>
-      </v-bottom-sheet>
+      <TypeSelection
+          v-model="isAddHeuristicSheetOpen"
+          :tab-items="heuristicTabItems"
+          :descriptors="heuristicDescriptors"
+          v-on:add-heuristic="addNewHeuristic"
+      />
       <Details
           v-model="heuristicSheet.isOpen"
           :heuristic-data="heuristicSheet"
@@ -186,6 +131,7 @@ import {
   mdiSourceBranchCheck, mdiDelete, mdiChartBar, mdiShapeSquarePlus, mdiDotsVertical,
   mdiAlertOctagon,
 } from '@mdi/js';
+import TypeSelection from './TypeSelection.vue';
 import Details from './Details.vue';
 import {
   ROUTE_NAME_TRANSACTION_PAGE, ROUTE_EXECUTE_HEURISTICS,
@@ -262,7 +208,7 @@ function areDataElementsEqual(a, b) {
 
 export default {
   name: 'Editor',
-  components: { Details, NestedMenu },
+  components: { TypeSelection, Details, NestedMenu },
   data() {
     return {
       icon: {
@@ -317,7 +263,6 @@ export default {
       changeSet: [],
       // deletedData holds all uids of the heuristic which are deleted
       deletedData: [],
-      isHeuristicSheetFixed: false,
       isAddHeuristicSheetOpen: false,
       // heuristicDetailsMap: map[heuristicUid]map[addressHash]array[originHash]
       heuristicDetailsMap: new Map(),
@@ -329,16 +274,7 @@ export default {
         resultCount: null,
       },
       heuristicDescriptors: [],
-      heuristicTabs: null,
       heuristicTabItems: [],
-      parameterRules: new Map([
-        ['int', [(v) => {
-          if (!/^\d+$/.test(v)) return false;
-          const num = parseInt(v, 10);
-          return Number.isInteger(num) && num > 0;
-        }]],
-        // string rule is not implemented yet
-        ['string', null]]),
       contextMenu: {
         display: false,
         x: 0,
@@ -409,14 +345,6 @@ export default {
     },
     setInfoMessage(msg) {
       this.$store.dispatch('addMessage', { text: msg, type: 'info', temporary: true });
-    },
-    addNewHeuristicAction(item) {
-      if (item.parameter !== undefined && !item.parameter.valid) {
-        return;
-      }
-      if (!this.isHeuristicSheetFixed) this.isAddHeuristicSheetOpen = false;
-
-      this.addNewHeuristic(item);
     },
     addNewHeuristic(heuristic) {
       const newHeuristic = { type: heuristic.type, uid: `${this.newUidPrefix}${this.uidCounter}` };
@@ -622,8 +550,6 @@ export default {
 
       this.data = { heuristics: updatedData, status: 0 };
 
-      // this.$store.dispatch('setHeuristicData', );
-
       const newStateMap = new Map(this.data.heuristics.map((d) => [d.uid, d]));
       this.deletedData = getDeletedData(this.dbState, newStateMap);
 
@@ -697,6 +623,23 @@ export default {
           this.setErrorMessage(error);
         });
     },
+    createTabs() {
+      const tabSet = new Set();
+      let isCategoryEmpty = false;
+      this.heuristicDescriptors.forEach((e) => {
+        if (e.category) {
+          tabSet.add(e.category);
+        } else {
+          // if no category is set
+          isCategoryEmpty = true;
+        }
+      });
+      this.heuristicTabItems = Array.from(tabSet).sort();
+
+      if (isCategoryEmpty) {
+        this.heuristicTabItems.push('Other');
+      }
+    },
     async onMounted() {
       const svgCanvasId = 'svg_canvas';
       // remove previous svg children
@@ -714,24 +657,12 @@ export default {
       if (!ht.setContextMenuCallback(this.showContextMenu)) {
         this.setErrorMessage('error setting context menu handler');
       }
+
+      // gets all heuristic type configurations
       await this.getDescriptors();
 
-      // get tab descriptions
-      const tabSet = new Set();
-      let isCategoryEmpty = false;
-      this.heuristicDescriptors.forEach((e) => {
-        if (e.category) {
-          tabSet.add(e.category);
-        } else {
-          // if no category is set
-          isCategoryEmpty = true;
-        }
-      });
-      this.heuristicTabItems = Array.from(tabSet).sort();
-
-      if (isCategoryEmpty) {
-        this.heuristicTabItems.push('Other');
-      }
+      // creates the tab descriptions based on the heuristic categories
+      this.createTabs();
 
       ht.setupSvg(this, svgCanvasId, this.heuristicDescriptors);
       await this.refreshData();
---
+++ app/src/components/heuristic/TypeSelection.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-bottom-sheet scrollable v-model="inputVal">
+    <v-card>
+      <div class="d-flex" style="align-items: center">
+        <div class="mr-auto ml-2 text-h6">
+          Add heuristic</div>
+        <v-switch
+            class="mr-2"
+            v-model="isHeuristicSheetFixed"
+            label="Fixed"
+        ></v-switch>
+      </div>
+      <v-card-text style="height: 80%">
+        <v-tabs v-model="heuristicTabs" v-if="tabItems">
+          <v-tab v-for="item in tabItems" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="heuristicTabs" class="mt-3">
+          <v-tab-item transition="fade-transition"
+                      v-for="item in tabItems"
+                      :key="item">
+            <div class="d-flex flex-wrap" style="align-items: flex-start;">
+              <v-card
+                  outlined
+                  v-for="(item, index)
+                      in descriptors.filter((e) => {
+                        if(!e.category && item === 'Other')
+                          return true;
+                        return e.category === item
+                      })"
+                  :key="index"
+                  class="mx-3 mb-6"
+                  max-width="300">
+                <v-card-title>
+                  {{ item.title }}
+                </v-card-title>
+                <v-card-subtitle>
+                  {{ item.description }}
+                </v-card-subtitle>
+                <v-card-subtitle>
+                  <v-form v-model="item.parameter.valid" v-if="item.parameter !== undefined">
+                    <v-text-field
+                        v-model="item.parameter.value"
+                        :rules="parameterRules.get(item.parameter.type)"
+                        :label="item.parameter.description"
+                        required>
+                    </v-text-field>
+                  </v-form>
+                </v-card-subtitle>
+                <v-card-actions class="pt-0">
+                  <v-btn outlined color="primary" @click="addNewHeuristicAction(item)">
+                    Add Heuristic
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </div>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card-text>
+    </v-card>
+  </v-bottom-sheet>
+</template>
+
+<script>
+export default {
+  name: 'TypeSelection',
+  props: {
+    value: { type: Boolean, required: true },
+    tabItems: { type: Array, required: true },
+    descriptors: { type: Array, required: true },
+    // events: add-heuristic
+  },
+  data() {
+    return {
+      isHeuristicSheetFixed: false,
+      heuristicTabs: null,
+      parameterRules: new Map([
+        ['int', [(v) => {
+          if (!/^\d+$/.test(v)) return false;
+          const num = parseInt(v, 10);
+          return Number.isInteger(num) && num > 0;
+        }]],
+        // string rule is not implemented yet
+        ['string', null]]),
+    };
+  },
+  computed: {
+    inputVal: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      },
+    },
+  },
+  methods: {
+    addNewHeuristicAction(item) {
+      if (item.parameter !== undefined && !item.parameter.valid) {
+        return;
+      }
+      if (!this.isHeuristicSheetFixed) this.inputVal = false;
+      this.$emit('add-heuristic', item);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
