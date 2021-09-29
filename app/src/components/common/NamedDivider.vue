8b9e02aac3470a41faa9cf2791938962db48c39a
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-09-29 16:01:18 +0200

    actually print title of named divider

--- app/src/components/common/NamedDivider.vue
+++ app/src/components/common/NamedDivider.vue
@@ -2,7 +2,7 @@
   <div :class="`my-${verticalMargin}`">
     <div class="d-flex align-center">
       <v-sheet color="grey lighten-2" height="1" width="100%"/>
-      <div class="mx-2" v-if="title">Or</div>
+      <div class="mx-2" v-if="title" style="white-space: nowrap;">{{ title }}</div>
       <v-sheet color="grey lighten-2" height="1" width="100%" v-if="title"/>
     </div>
   </div>
