73e0412b71d4faceb85da9b6da8170221b55c2e9
Michael Ziegler <michael.h.ziegler@ntnu.no>
2021-12-10 12:07:20 +0100

    fix dialog title

--- app/src/components/tools/AddCluster.vue
+++ app/src/components/tools/AddCluster.vue
@@ -1,73 +1,69 @@
 <template>
   <v-dialog v-model="show" max-width="700px">
-  <v-card
-      class="mx-auto elevation-4">
-    <v-toolbar color="primary" dark flat>
-      <v-toolbar-title>
-        <v-icon>{{ icon.mdiMerge }}</v-icon>
-        Add Cluster
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-text>
-      <div class="text-subtitle-1">
-        Add custom address clusters by uploading a CSV-file.
-        The file must have two columns, where the first column contains an
-        identifier for each cluster and the second column the addresses.
-      </div>
-      <v-expansion-panels flat>
-        <v-expansion-panel>
-          <v-expansion-panel-header>
-            Example CSV-file
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <p>The following file content would generate two clusters with two addresses each.</p>
-            <pre><code>cluster-id,address
+    <v-card class="mx-auto elevation-4">
+      <v-card-title>
+        <span class="text-h5">Add Cluster</span>
+      </v-card-title>
+      <v-card-text>
+        <div class="text-subtitle-1">
+          Add custom address clusters by uploading a CSV-file.
+          The file must have two columns, where the first column contains an
+          identifier for each cluster and the second column the addresses.
+        </div>
+        <v-expansion-panels flat>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              Example CSV-file
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <p>The following file content would generate two clusters with two addresses each.</p>
+              <pre><code>cluster-id,address
 1,XgG6Nosmei5woQ2VTDzwmLX7SzdNYKHdiz
 1,Xf36MqBkoK8G5wBbjUSwDRy6XTjdNq8hgB
 2,XatWuw7BhTxHvjPLbnvPArWgW9r6hjpt8o
 2,XcsCPgY67TqW9CpsJLCbizDw2Yq2zFoh74</code></pre>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-      <v-form ref="csvForm" id="csvForm">
-        <v-file-input
-            v-model="csv.file"
-            :rules="rules.file"
-            show-size
-            accept="text/csv"
-            label="Click here to select a file"
-            truncate-length="15"/>
-        <v-row>
-          <v-col>
-            <v-switch v-model="csv.firstRowContainsHeader"
-                      label="First row of file contains headers" :disabled="isLoading"/>
-          </v-col>
-          <v-col>
-            <v-select
-                v-model="csv.separator"
-                :items="separatorItems"
-                item-text="text"
-                item-value="value"
-                label="Separator">
-            </v-select>
-          </v-col>
-          <v-col class="d-flex justify-end align-center">
-            <v-btn
-                color="primary"
-                :loading="isLoading"
-                @click="handleCSVUpload">
-              Upload
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-form>
-    </v-card-text>
-  </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+        <v-form ref="csvForm" id="csvForm">
+          <v-file-input
+              v-model="csv.file"
+              :rules="rules.file"
+              show-size
+              accept="text/csv"
+              label="Click here to select a file"
+              truncate-length="15"/>
+          <v-row>
+            <v-col>
+              <v-switch v-model="csv.firstRowContainsHeader"
+                        label="First row of file contains headers" :disabled="isLoading"/>
+            </v-col>
+            <v-col>
+              <v-select
+                  v-model="csv.separator"
+                  :items="separatorItems"
+                  item-text="text"
+                  item-value="value"
+                  label="Separator">
+              </v-select>
+            </v-col>
+            <v-col class="d-flex justify-end align-center">
+              <v-btn text :disabled="isLoading" class="mr-2" @click="show = false">
+                Cancel
+              </v-btn>
+              <v-btn text :loading="isLoading" @click="handleCSVUpload">
+                Upload
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
   </v-dialog>
 </template>
 
 <script>
-import { mdiFileDownloadOutline, mdiMerge } from '@mdi/js';
+import { mdiFileDownloadOutline } from '@mdi/js';
 import { ROUTE_ADD_CLUSTER } from '../../constants';
 import { doPostUpload } from '../../utilities';
 
@@ -104,9 +100,7 @@ export default {
   },
   data() {
     return {
-      icon: {
-        mdiMerge, mdiFileDownloadOutline,
-      },
+      icon: { mdiFileDownloadOutline },
       isLoading: false,
       separatorItems: [
         { text: 'Colon (,)', value: ',' },
