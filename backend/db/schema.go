8e9f2f7e5536ab981a18ef6a51c3e425b5497996
Michael Ziegler <michael.h.ziegler@ntnu.no>
2022-01-20 10:08:35 +0100

    move predicate definitions close to type definitions and move db upgrade functions to seperate file

--- backend/db/schema.go
+++ backend/db/schema.go
@@ -16,54 +16,10 @@ func SetupSchema(c external.Database) error {
 	return c.Alter(context.Background(), &api.Operation{
 		Schema: `
 			blockhash: string @index(hash) @upsert .
-			txhash: string @index(hash) @upsert .
-			addresshash: string @index(hash) @upsert .
-
-			tx_inputs: [uid] @reverse .
-			tx_outputs: [uid] @reverse .
-			addr_outputs: [uid] @reverse .
-			transactions: [uid] @reverse .
-			results: [uid] @count @reverse .
-			prevblock: uid @reverse .
-			h_transaction: uid @reverse .
-			parent_heuristic: [uid] @reverse .
-			
 			id: int @index(int) .
 			ts: dateTime @index(day) .
-			outputindex: int .
-			inputindex: int .
-			txtype: string .
-			amount: int .
-			fee: int .
-			iscoinbase: bool .
-			privacytype: int @index(int) .
-			isrlookupdone: bool @index(bool) .
-			keyasm: string @index(term) .
-			sigasm: string .
-			keyhex: string .
-			sighex: string .
-
-			iscrawling: bool .
-			isclassifying: bool .
-			isclustering: bool .
-			lastblockid: int .
-			lastclassifiedid: int .
-			lastclusteredid: int .
-
-			type: string @index(hash) .
-			parameter: string .
-
-			role_name: string @index(hash) .
-
-			user_email: string @index(term, fulltext) .
-			user_pwhash: string .
-			user_roles: [uid] @reverse .
-			user_created: dateTime @index(day) .
-			user_modified: dateTime @index(day) .
-			user_heuristics: [uid] @reverse .
-
-			origin: uid @reverse .
-			destinations: [uid] @reverse .
+			prevblock: uid @reverse .
+			transactions: [uid] @reverse .
 
 			type Block {
 				blockhash
@@ -74,6 +30,13 @@ func SetupSchema(c external.Database) error {
 				transactions
 			}
 
+			txhash: string @index(hash) @upsert .
+			privacytype: int @index(int) .
+			isrlookupdone: bool @index(bool) .
+			fee: int .
+			tx_inputs: [uid] @reverse .
+			tx_outputs: [uid] @reverse .
+
 			type Transaction {
 				txhash
 				privacytype
@@ -83,6 +46,16 @@ func SetupSchema(c external.Database) error {
 				tx_outputs
 				tx_inputs
 			}
+
+			outputindex: int .
+			inputindex: int .
+			txtype: string .
+			amount: int .
+			iscoinbase: bool .
+			keyasm: string @index(term) .
+			sigasm: string .
+			keyhex: string .
+			sighex: string .
 
 			type Output {
 				outputindex
@@ -98,21 +71,33 @@ func SetupSchema(c external.Database) error {
 				<~tx_outputs>
 				<~addr_outputs>
 			}
-			
+
+			addresshash: string @index(hash) @upsert .
+			addr_outputs: [uid] @reverse .
+
 			type Address {
 				addresshash
 				addr_outputs
 			}
+
+			iscrawling: bool .
+			lastblockid: int .
 
 			type CrawlerStatus {
 				iscrawling
 				lastblockid
 			}
 
+			isclassifying: bool .
+			lastclassifiedid: int .
+
 			type ClassifierStatus {
 				isclassifying
 				lastclassifiedid
 			}
+
+			isclustering: bool .
+			lastclusteredid: int .
 
 			# Clustering Hierarchical Multi Input Status
 			type CHMIStatus {
@@ -126,6 +111,12 @@ func SetupSchema(c external.Database) error {
 				lastclusteredid
 			}
 
+			type: string @index(hash) .
+			parameter: string .
+			h_transaction: uid @reverse .
+			results: [uid] @count @reverse .
+			parent_heuristic: [uid] @reverse .
+
 			type TransactionHeuristic {
 				type
 				parameter
@@ -135,14 +126,26 @@ func SetupSchema(c external.Database) error {
 				parent_heuristic
 			}
 
+			origin: uid @reverse .
+			destinations: [uid] @reverse .
+
 			type TransactionHeuristicResult {
 				origin
 				destinations
 			}
 
+			role_name: string @index(hash) .
+
 			type Role {
 				role_name
 			}
+
+			user_email: string @index(term, fulltext) .
+			user_pwhash: string .
+			user_roles: [uid] @reverse .
+			user_created: dateTime @index(day) .
+			user_modified: dateTime @index(day) .
+			user_heuristics: [uid] @reverse .
 
 			type User {
 				user_email
@@ -188,10 +191,12 @@ func SetupSchema(c external.Database) error {
 
 // IsSchemaSet checks if a schema is set
 func IsSchemaSet(c external.Database) (exists bool, err error) {
-	query := "schema(type: Block){}"
 	ctx, cancel := GetBackendContext()
 	defer cancel()
-	resp, err := c.Query(ctx, query, nil)
+
+	// we check if the type Block exists,
+	// as this is the most basic type and thus should exist in all schemas
+	resp, err := c.Query(ctx, "schema(type: Block){}", nil)
 	if err != nil {
 		err = fmt.Errorf("%s: %w", cliutil.ShowCallInfo(), err)
 		return
@@ -215,215 +220,4 @@ func IsSchemaSet(c external.Database) (exists bool, err error) {
 	}
 
 	return
-}
-
-// AlterSchemaAddClassifier adds the new classifier status field
-func AlterSchemaAddClassifier(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			isclassifying: bool .
-			lastclassifiedid: int .
-
-			type ClassifierStatus {
-				isclassifying
-				lastclassifiedid
-			}
-		`,
-	})
-}
-
-// AlterSchemaChangePrivacyTypePredicate adds the privacy type predicate
-func AlterSchemaChangePrivacyTypePredicate(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			privacytype: int @index(int) .
-		`,
-	})
-}
-
-// DropAllPrivacyTypes drops the all data of the predicate privacytype
-func DropAllPrivacyTypes(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropAttr: "privacytype",
-	})
-}
-
-// DropAllOrigins removes all data from the predicate origins
-func DropAllOrigins(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropAttr: "origins",
-	})
-}
-
-// DropIsAnalyzing removes all data from the predicate isanalyzing
-func DropIsAnalyzing(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropAttr: "isanalyzing",
-	})
-}
-
-// DropLastAnalysedID removes all data from the predicate lastanalysedid
-func DropLastAnalysedID(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropAttr: "lastanalysedid",
-	})
-}
-
-// DropTypeAnalyzerStatus removes the type AnalyzerStatus
-func DropTypeAnalyzerStatus(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropOp:    api.Operation_TYPE,
-		DropValue: "AnalyzerStatus",
-	})
-}
-
-// DropTypeOldClusterStatusStatus removes the old cluster status type
-func DropTypeOldClusterStatusStatus(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropOp:    api.Operation_TYPE,
-		DropValue: "CMultiInputStatus",
-	})
-}
-
-// AlterSchemaSetTransactionType adds the privacytype predicate to the transaction type
-func AlterSchemaSetTransactionType(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			type Transaction {
-				txhash
-				privacytype
-				isrlookupdone
-				fee
-				<~transactions>
-				tx_outputs
-				tx_inputs
-			}
-		`,
-	})
-}
-
-// AlterSchemaAddHeuristicResult adds the heuristic result type
-func AlterSchemaAddHeuristicResult(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			origin: uid @reverse .
-			destinations: [uid] @reverse .
-
-			type TransactionHeuristicResult {
-				origin
-				destinations
-			}
-		`,
-	})
-}
-
-// AlterSchemaAddHierarchicalMultiInputClusteringStatus adds the new multi-input clustering status field
-func AlterSchemaAddHierarchicalMultiInputClusteringStatus(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			isclustering: bool .
-			lastclusteredid: int .
-
-			# Clustering Hierarchical Multi Input Status
-			type CHMIStatus {
-				isclustering
-				lastclusteredid
-			}
-		`,
-	})
-}
-
-// AlterSchemaAddFlatMultiInputClusteringStatus adds the new multi-input clustering status field
-func AlterSchemaAddFlatMultiInputClusteringStatus(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			isclustering: bool .
-			lastclusteredid: int .
-			
-			# Clustering Flat Multi Input Status
-			type CFMIStatus {
-				isclustering
-				lastclusteredid
-			}
-		`,
-	})
-}
-
-// AlterSchemaAddMultiInputClusterType adds the new multi-input cluster type
-func AlterSchemaAddMultiInputClusterType(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			cluster_type: string @index(hash) . # the cluster type
-			cluster_transaction: uid @reverse . # the transaction which contains the address because of which the cluster was created
-			cluster_addresses: [uid] @reverse . # all direct addresses, these occur in cluster_transaction
-			cluster_children: [uid] @reverse . # all direct child clusters
-			cluster_address_count: int . # number of connected addresses connected to this cluster (including child clusters)
-
-			type Cluster {
-				cluster_type
-				cluster_transaction
-				cluster_addresses
-				cluster_children
-				cluster_address_count
-			}
-		`,
-	})
-}
-
-// AlterSchemaRemoveLowestBlockIdFromCrawlerStatus removes the lowestblock id from the crawler status
-func AlterSchemaRemoveLowestBlockIdFromCrawlerStatus(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			type CrawlerStatus {
-				iscrawling
-				lastblockid
-			}
-		`,
-	})
-}
-
-// DropLowestBlockId removes all data from the predicate lowestblockid
-func DropLowestBlockId(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		DropAttr: "lowestblockid",
-	})
-}
-
-// AlterSchemaAddUserToCluster adds the user predicate to the cluster type
-func AlterSchemaAddUserToCluster(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			cluster_user: uid @reverse . # the user which created the cluster
-			cluster_ts: dateTime @index(day).
-			
-			type Cluster {
-				cluster_type
-				cluster_transaction
-				cluster_addresses
-				cluster_children
-				cluster_address_count
-				cluster_user
-				cluster_ts
-			}
-		`,
-	})
-}
-
-// AlterSchemaAddAttribution adds the attribution type
-func AlterSchemaAddAttribution(c external.Database) error {
-	return c.Alter(context.Background(), &api.Operation{
-		Schema: `
-			attribution_user: uid @reverse . # the user which created the attribution
-			attribution_tag: string @index(term, trigram) .
-			attribution_address: uid @reverse . # the attribution's address
-			attribution_ts: dateTime @index(day). # creation time of the attribution
-
-			type Attribution {
-				attribution_user
-				attribution_tag
-				attribution_address
-				attribution_ts
-			}
-		`,
-	})
 }
