1792bc5a3d7140265486901510ef1a98c84eda09
Michael Ziegler <michael.h.ziegler@ntnu.no>
2022-09-05 10:34:09 +0200

    add label to image and text node

--- app/src/components/user/flows/OryUiNode.vue
+++ app/src/components/user/flows/OryUiNode.vue
@@ -5,11 +5,15 @@
         :attributes="node.attributes"
         :meta="node.meta"
         :id="id" @submit="propagateSubmitEvent"/>
-    <v-img v-else-if="isUiNodeImageAttributes(node.attributes)"
-           :src="node.attributes.src"
-           :id="id"
-           :width="node.attributes.witdh"
-           :height="node.attributes.height"
+    <v-img
+        class="ma-3"
+        v-else-if="isUiNodeImageAttributes(node.attributes)"
+        contain
+        :src="node.attributes.src"
+        :id="id"
+        :width="node.attributes.witdh"
+        :height="node.attributes.height"
+        :alt="node.meta.label.text"
     />
     <a v-else-if="isUiNodeAnchorAttributes(node.attributes)" :href="node.attributes.href">
       {{ node.attributes.title.text }}
@@ -22,7 +26,14 @@
         :referrerpolicy="node.attributes.referrerpolicy"
         :crossorigin="node.attributes.crossorigin"
     ></script>
-    <p v-else-if="isUiNodeTextAttributes(node.attributes)">{{ node.attributes.text.text }}</p>
+    <template v-else-if="isUiNodeTextAttributes(node.attributes)">
+      <p v-if="node.meta && node.meta.label && node.meta.label.text">
+        {{ `${node.meta.label.text}:` }}
+      </p>
+      <p v-if="node.attributes && node.attributes.text && node.attributes.text.text">
+        {{ node.attributes.text.text }}
+      </p>
+    </template>
     <v-btn v-else-if="node.type === 'submit'"></v-btn>
     <template v-if="node.messages">
       <ory-ui-message v-for="(msg,i) in node.messages" :key="i" :message="msg"/>
