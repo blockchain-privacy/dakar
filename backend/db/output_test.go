9f424217ff3876b08ecb3a7bee2cd8e71c7509f6
Michael Ziegler <michael.h.ziegler@ntnu.no>
2022-09-26 13:04:41 +0200

    more output tests

--- backend/db/output_test.go
+++ backend/db/output_test.go
@@ -5,6 +5,19 @@ import (
 	"testing"
 )
 
+func TestOutput_String(t *testing.T) {
+	output := Output{}
+	require.NotEmpty(t, output.String())
+	output.Amount = getInt64Pointer(500)
+	require.NotEmpty(t, output.String())
+}
+
+func TestOutput_SetDType(t *testing.T) {
+	output := Output{}
+	output.SetDType()
+	require.Equal(t, []string{outputDType}, output.DType)
+}
+
 func TestCountAmountDenominations(t *testing.T) {
 	type testCase struct {
 		amounts []int64
