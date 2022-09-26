69d49f5fb22977971ea8966729508f2a3848e0c4
Michael Ziegler <michael.h.ziegler@ntnu.no>
2022-09-26 13:04:47 +0200

    more db tests

--- backend/db/db_test.go
+++ backend/db/db_test.go
@@ -2,14 +2,58 @@ package db
 
 import (
 	"backend/testhelper"
+	"github.com/dgraph-io/dgo/v210/protos/api"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 const blockFileName = "testdata/blocks_60000_60020.json"
 
 func TestMain(m *testing.M) {
 	RunDgraphTests(m, &dbHandle, ContainerNameDB)
+}
+
+func TestInfo(t *testing.T) {
+	require.NotPanics(t, func() {
+		info("test123")
+		info()
+		info(-1)
+		info(nil)
+	})
+}
+
+func TestGetBackendContext(t *testing.T) {
+	require.NotPanics(t, func() {
+		_, cancel := GetBackendContext()
+		cancel()
+	})
+}
+
+func TestGetFrontendContext(t *testing.T) {
+	require.NotPanics(t, func() {
+		_, cancel := GetFrontendContext()
+		cancel()
+	})
+}
+
+func TestExecTx(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+
+	_, err := execTx(dbHandle, time.Duration(0), &api.Request{
+		Query:     `{q(func:uid(0x1)){uid}}`,
+		CommitNow: true,
+	})
+	require.Error(t, err)
+
+	_, err = execTx(dbHandle, time.Minute, nil)
+	require.Error(t, err)
+
+	_, err = execTx(dbHandle, time.Minute, &api.Request{
+		Query:     `{q(func:uid(0x1)){uid}}`,
+		CommitNow: true,
+	})
+	require.NoError(t, err)
 }
 
 func TestCreateCommaList(t *testing.T) {
