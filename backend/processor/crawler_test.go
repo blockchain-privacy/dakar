f4294d74b6f57b53009f305be701b310d616d674
Michael Ziegler <michael.h.ziegler@ntnu.no>
2022-10-08 13:02:53 +0200

    refine crawler tests

--- backend/processor/crawler_test.go
+++ backend/processor/crawler_test.go
@@ -1,6 +1,8 @@
 package processor
 
 import (
+	"backend/db"
+	"backend/testhelper"
 	"context"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
@@ -50,19 +52,104 @@ func TestCrawler_DB(t *testing.T) {
 }
 
 func TestCrawler_IncrementState(t *testing.T) {
-	crawler := NewCrawler(context.Background(), nil, nil, nil, 0, Config{})
+	testhelper.SkipIfNotCI(t)
+	db.SetupDBWithoutData(t, dbHandle)
+
+	crawler := NewCrawler(context.Background(), dbHandle, client, batchClient, 0, NewBitcoinConfig())
 	unregisterCollectors(crawler)
 
 	// initial state is not set yet
 	require.Error(t, crawler.IncrementState())
 
-	// todo use CalculateInitialState and call method again
+	require.NoError(t, crawler.CalculateInitialState())
+	block, err := crawler.NextBlock()
+	require.NoError(t, err)
+	require.True(t, block)
+
+	require.NoError(t, crawler.IncrementState())
 }
 
 func TestCrawler_Empty(t *testing.T) {
-	crawler := NewCrawler(context.Background(), nil, nil, nil, 0, Config{})
+	testhelper.SkipIfNotCI(t)
+	db.SetupDBWithoutData(t, dbHandle)
+
+	crawler := NewCrawler(context.Background(), dbHandle, client, batchClient, 0, NewBitcoinConfig())
 	unregisterCollectors(crawler)
 	require.True(t, crawler.Empty())
 
-	// todo use CalculateInitialState and call method again
+	require.NoError(t, crawler.CalculateInitialState())
+	isNewBlock, err := crawler.NextBlock()
+	require.NoError(t, err)
+	require.True(t, isNewBlock)
+
+	// make block height higher so Empty() returns false
+	// This is needed because the testrpc client has fewer blocks than the ForkRangeLimit
+	crawler.state.top = 1000000
+
+	require.False(t, crawler.Empty())
+}
+
+func TestCrawler_CalculateInitialState(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDBWithoutData(t, dbHandle)
+
+	crawler := NewCrawler(context.Background(), dbHandle, client, batchClient, 0, NewBitcoinConfig())
+	unregisterCollectors(crawler)
+	require.NoError(t, crawler.CalculateInitialState())
+}
+
+func TestCrawler_PostExecution(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDBWithoutData(t, dbHandle)
+
+	crawler := NewCrawler(context.Background(), dbHandle, client, batchClient, 0, NewBitcoinConfig())
+	unregisterCollectors(crawler)
+
+	require.NoError(t, crawler.PostExecution())
+}
+
+func TestCrawler_CurrentBlock(t *testing.T) {
+	crawler := NewCrawler(context.Background(), nil, nil, nil, 0, Config{})
+	unregisterCollectors(crawler)
+	require.Zero(t, crawler.CurrentBlock())
+}
+
+func TestCrawler_NextBlock(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDBWithoutData(t, dbHandle)
+
+	crawler := NewCrawler(context.Background(), dbHandle, client, batchClient, 0, NewBitcoinConfig())
+	unregisterCollectors(crawler)
+
+	_, err := crawler.NextBlock()
+	require.Error(t, err)
+
+	require.NoError(t, crawler.CalculateInitialState())
+	isNewBlock, err := crawler.NextBlock()
+	require.NoError(t, err)
+	require.True(t, isNewBlock)
+}
+
+func TestCrawler_Iterate(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDBWithoutData(t, dbHandle)
+
+	crawler := NewCrawler(context.Background(), dbHandle, client, batchClient, 0, NewBitcoinConfig())
+	unregisterCollectors(crawler)
+
+	_, err := crawler.Iterate()
+	require.Error(t, err)
+
+	require.NoError(t, crawler.CalculateInitialState())
+	isNewBlock, err := crawler.NextBlock()
+	require.NoError(t, err)
+	require.True(t, isNewBlock)
+
+	// make block height higher so Empty() returns false.
+	// This is needed because the testrpc client has fewer blocks than the ForkRangeLimit
+	crawler.state.top = 1000000
+
+	success, err := crawler.Iterate()
+	require.NoError(t, err)
+	require.True(t, success)
 }
