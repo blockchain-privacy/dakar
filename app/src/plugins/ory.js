59a8ea6e7e542dba7c573de7cddf8137514050fd
Michael Ziegler <michael.h.ziegler@ntnu.no>
2022-12-05 22:59:48 +0100

    upgrade ory sdk on frontend

--- app/src/plugins/ory.js
+++ app/src/plugins/ory.js
@@ -1,16 +1,24 @@
-import { Configuration, V0alpha2Api } from '@ory/client';
+import {
+  Configuration, OAuth2Api, IdentityApi, FrontendApi,
+} from '@ory/client';
 import { ORY_KRATOS_PATH_PREFIX } from '../constants';
+
+const config = new Configuration({
+  basePath: ORY_KRATOS_PATH_PREFIX,
+  baseOptions: {
+    withCredentials: true,
+    headers: {
+      Accept: 'application/json',
+    },
+  },
+});
 
 export default {
   install(Vue) {
-    Vue.prototype.ory = new V0alpha2Api(new Configuration({
-      basePath: ORY_KRATOS_PATH_PREFIX,
-      baseOptions: {
-        withCredentials: true,
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-    }));
+    Vue.prototype.ory = {
+      identity: new IdentityApi(config),
+      frontend: new FrontendApi(config),
+      oauth2: new OAuth2Api(config),
+    };
   },
 };
