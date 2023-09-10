d20f33044748e9cabb9315b050c3c3453f6e1c4c
Michael Ziegler <michael.h.ziegler@ntnu.no>
2023-09-10 13:46:47 +0200

    add import tests

---
+++ backend/analytics/graph/import_test.go
@@ -0,0 +1,203 @@
+package graph
+
+import (
+	"backend/db/analytics"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func Test_addSingleNodes(t *testing.T) {
+	g := NewReversibleGraph(10)
+	tests := []struct {
+		nodes   []analytics.Node
+		wantErr bool
+	}{
+		{
+			nodes: []analytics.Node{{
+				UID: "0x123",
+				Block: []struct {
+					TS time.Time `json:"ts"`
+				}{{TS: time.Now()}},
+			}},
+			wantErr: false,
+		},
+		{
+			nodes:   []analytics.Node{{UID: "12", Block: nil}},
+			wantErr: true,
+		},
+		{
+			nodes: nil,
+		},
+	}
+	for _, tt := range tests {
+		if tt.wantErr {
+			require.Error(t, addSingleNodes(g, tt.nodes))
+		} else {
+			require.NoError(t, addSingleNodes(g, tt.nodes))
+		}
+	}
+}
+
+func Test_upsertSingleNodes(t *testing.T) {
+	g := NewReversibleGraph(10)
+	tests := []struct {
+		nodes   []analytics.Node
+		wantErr bool
+	}{
+		{
+			nodes: []analytics.Node{{
+				UID: "0x123",
+				Block: []struct {
+					TS time.Time `json:"ts"`
+				}{{TS: time.Now()}},
+			}},
+			wantErr: false,
+		},
+		{
+			nodes: []analytics.Node{{
+				UID:   "12",
+				Block: nil,
+			}},
+			wantErr: true,
+		},
+		{
+			nodes: nil,
+		},
+	}
+	for _, tt := range tests {
+		if tt.wantErr {
+			require.Error(t, upsertSingleNodes(g, tt.nodes))
+		} else {
+			require.NoError(t, upsertSingleNodes(g, tt.nodes))
+		}
+	}
+}
+
+func Test_addEdges(t *testing.T) {
+	g := NewReversibleGraph(10)
+	tests := []struct {
+		nodes   []analytics.ConnectedNode
+		wantErr bool
+	}{
+		{
+			nodes: []analytics.ConnectedNode{{
+				UID:    "0x123",
+				Inputs: nil,
+			}},
+			wantErr: false,
+		},
+		{
+			nodes: []analytics.ConnectedNode{{
+				UID: "0x1234",
+				Inputs: []struct {
+					Address          string
+					InputTransaction string
+				}{{
+					Address:          "",
+					InputTransaction: "",
+				}},
+			}},
+			wantErr: true,
+		},
+		{
+			nodes: []analytics.ConnectedNode{{
+				UID: "0x1235",
+				Inputs: []struct {
+					Address          string
+					InputTransaction string
+				}{{
+					Address:          "0x1",
+					InputTransaction: "",
+				}},
+			}},
+			wantErr: true,
+		},
+		{
+			nodes: []analytics.ConnectedNode{{
+				UID: "0x1235",
+				Inputs: []struct {
+					Address          string
+					InputTransaction string
+				}{{
+					Address:          "",
+					InputTransaction: "0x1",
+				}},
+			}},
+			wantErr: true,
+		},
+		{
+			nodes: []analytics.ConnectedNode{{
+				UID: "0x1235",
+				Inputs: []struct {
+					Address          string
+					InputTransaction string
+				}{{
+					Address:          "0x1",
+					InputTransaction: "0x2",
+				}},
+			}},
+			wantErr: false,
+		},
+		{
+			nodes: []analytics.ConnectedNode{{
+				UID: "12",
+			}},
+			wantErr: true,
+		},
+		{
+			nodes: nil,
+		},
+	}
+	for _, tt := range tests {
+		if tt.wantErr {
+			require.Error(t, addEdges(g, tt.nodes))
+		} else {
+			require.NoError(t, addEdges(g, tt.nodes))
+		}
+	}
+}
+
+func Test_pruneNodes(t *testing.T) {
+	g := NewReversibleGraph(1)
+	require.NoError(t, pruneNodes(g))
+
+	// unset time should cause error
+	g.AddNode(TransactionNode{
+		TS:          time.Time{},
+		id:          0,
+		PrivacyType: 0,
+	})
+	require.Error(t, pruneNodes(g))
+
+	// pruning should have not an effect, as all nodes are connected
+	g = newTestGraph()
+	numNodes := g.Nodes().Len()
+	require.NoError(t, pruneNodes(g))
+	require.Equal(t, numNodes, g.Nodes().Len())
+
+	// add unconnected node, therefore pruning should show an effect
+	g.AddNode(TransactionNode{
+		TS: time.Now(),
+		id: 10000,
+	})
+	numNodes = g.Nodes().Len()
+	require.NoError(t, pruneNodes(g))
+	require.Equal(t, numNodes-1, g.Nodes().Len())
+}
+
+func Test_verifyTransactionGraph(t *testing.T) {
+	g := NewReversibleGraph(1)
+	require.NoError(t, verifyTransactionGraph(g))
+
+	// unset time should cause error
+	g.AddNode(TransactionNode{TS: time.Time{}})
+	require.Error(t, verifyTransactionGraph(g))
+
+	// add unconnected node, therefore pruning should show an effect
+	g.AddNode(TransactionNode{
+		TS: time.Now(),
+		id: 10000,
+	})
+	require.Error(t, verifyTransactionGraph(g))
+}
