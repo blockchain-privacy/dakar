6244980cb6f8eaa4808bf09ae66b8913b964b71d
Michael Ziegler <michael.h.ziegler@ntnu.no>
2023-09-17 00:50:52 +0200

    finish graph tests

--- backend/analytics/graph/import_test.go
+++ backend/analytics/graph/import_test.go
@@ -209,7 +209,7 @@ func Test_loadOriginTransactions(t *testing.T) {
 	g := NewReversibleGraph(1)
 	// testdata contains 1 origin transaction
 	db.SetupDB(t, dbHandle, testhelper.UsePrivacyFile)
-	require.Nil(t, loadOriginTransactions(dbHandle, g, 0))
+	require.NoError(t, loadOriginTransactions(dbHandle, g, 0))
 	require.Equal(t, 1, g.Nodes().Len())
 }
 
@@ -218,7 +218,7 @@ func Test_loadDestinationTransactions(t *testing.T) {
 	g := NewReversibleGraph(1)
 	// testdata contains 1 destination transaction with 7 input transactions
 	db.SetupDB(t, dbHandle, testhelper.UsePrivacyFile)
-	require.Nil(t, loadDestinationTransactions(dbHandle, g, 0))
+	require.NoError(t, loadDestinationTransactions(dbHandle, g, 0))
 	require.Equal(t, 8, g.Nodes().Len())
 }
 
@@ -227,6 +227,25 @@ func Test_loadMixingTransactions(t *testing.T) {
 	g := NewReversibleGraph(132)
 	// testdata contains 132 mixing transactions and 557 input transactions
 	db.SetupDB(t, dbHandle, testhelper.UsePrivacyFile)
-	require.Nil(t, loadMixingTransactions(dbHandle, g, 0))
+	require.NoError(t, loadMixingTransactions(dbHandle, g, 0))
 	require.Equal(t, 689, g.Nodes().Len())
+}
+
+func Test_loadCCTransactions(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	g := NewReversibleGraph(132)
+	// testdata contains 0 cc transactions
+	db.SetupDB(t, dbHandle, testhelper.UsePrivacyFile)
+	require.NoError(t, loadCCTransactions(dbHandle, g, 0))
+	require.Equal(t, 0, g.Nodes().Len())
+}
+
+func TestLoadTransactionGraph(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDB(t, dbHandle, testhelper.UsePrivacyFile)
+
+	graph, err := LoadTransactionGraph(dbHandle, 0)
+	require.NoError(t, err)
+	// mixing_count=132 origin_count=1 destination_count=1 cc_count=0
+	require.Equal(t, 134, graph.Nodes().Len())
 }
