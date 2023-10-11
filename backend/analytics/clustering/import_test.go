8ef5d7dffe8322e51f238b705c093a6ad604515f
Michael Ziegler <michael.h.ziegler@ntnu.no>
2023-10-11 19:19:44 +0200

    add tests for custom cluster import

---
+++ backend/analytics/clustering/import_test.go
@@ -0,0 +1,189 @@
+package clustering
+
+import (
+	"backend/db"
+	"backend/db/analytics/clustering"
+	"backend/testhelper"
+	"github.com/stretchr/testify/require"
+	"sort"
+	"strconv"
+	"testing"
+)
+
+func Test_buildClusterSet(t *testing.T) {
+	tests := []struct {
+		clusters []ExternalClusterItem
+		want     map[string]map[string]bool
+	}{
+		{
+			clusters: []ExternalClusterItem{},
+			want:     map[string]map[string]bool{},
+		},
+		{
+			clusters: []ExternalClusterItem{
+				{ClusterID: "1", AddressHash: "a"},
+				{ClusterID: "1", AddressHash: "b"},
+			},
+			want: map[string]map[string]bool{"1": {"a": true, "b": true}},
+		},
+		{
+			clusters: []ExternalClusterItem{
+				{ClusterID: "1", AddressHash: "a"},
+				{ClusterID: "2", AddressHash: "b"},
+				{ClusterID: "1", AddressHash: "c"},
+				{ClusterID: "2", AddressHash: "d"},
+				{ClusterID: "2", AddressHash: "e"},
+			},
+			want: map[string]map[string]bool{
+				"1": {"a": true, "c": true},
+				"2": {"b": true, "d": true, "e": true},
+			},
+		},
+	}
+	for _, tt := range tests {
+		require.Equal(t, tt.want, buildClusterSet(tt.clusters))
+	}
+}
+
+func Test_buildDatabaseClusters(t *testing.T) {
+	type args struct {
+		clusters  []ExternalClusterItem
+		hashToUID map[string]string
+	}
+	tests := []struct {
+		args args
+		want []clustering.CustomCluster
+	}{
+		{
+			args: args{clusters: nil, hashToUID: nil},
+			want: []clustering.CustomCluster{},
+		},
+		{
+			args: args{
+				clusters: []ExternalClusterItem{
+					{ClusterID: "1", AddressHash: "a"},
+					{ClusterID: "2", AddressHash: "b"},
+					{ClusterID: "1", AddressHash: "c"},
+					{ClusterID: "2", AddressHash: "d"},
+					{ClusterID: "2", AddressHash: "e"},
+				},
+				hashToUID: map[string]string{"a": "0x1", "b": "0x2", "c": "0x3", "d": "0x4", "e": "0x5"},
+			},
+			want: []clustering.CustomCluster{
+				{Addresses: []clustering.HollowAddress{{UID: "0x1"}, {UID: "0x3"}}},
+				{Addresses: []clustering.HollowAddress{{UID: "0x2"}, {UID: "0x4"}, {UID: "0x5"}}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		clusters := buildDatabaseClusters(tt.args.clusters, "0x1234", tt.args.hashToUID)
+
+		sort.Slice(clusters, func(i, j int) bool {
+			return *clusters[i].AddressCount < *clusters[j].AddressCount
+		})
+
+		for i, c := range clusters {
+			// sort slice so order is the same as in the expected result
+			sort.Slice(c.Addresses, func(i, j int) bool {
+				return c.Addresses[i].UID < c.Addresses[j].UID
+			})
+			require.Equal(t, tt.want[i].Addresses, c.Addresses)
+		}
+	}
+}
+
+func Test_validateAddresses(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDB(t, dbHandle, testhelper.UseBlockFile)
+
+	// generate 1001 address hashes
+	oneThousandAndOneAddresses := make([]ExternalClusterItem, 1001)
+	for i := 0; i < 1001; i++ {
+		oneThousandAndOneAddresses[i] = ExternalClusterItem{ClusterID: "a", AddressHash: strconv.Itoa(i)}
+	}
+
+	tests := []struct {
+		clusters    []ExternalClusterItem
+		wantMapLen  int
+		wantErr     bool
+		wantErrType error
+	}{
+		{clusters: nil, wantErr: true},
+		{
+			clusters: []ExternalClusterItem{
+				{ClusterID: "1", AddressHash: "XccPicDbg7HhRfFwPU3Z2CFVpHSYAhuxtu"},
+				{ClusterID: "2", AddressHash: "XggCE7j3QozdXavADMLVEmREPvKXKQexqP"},
+				{ClusterID: "1", AddressHash: "Xu4ebhFCAp8vq8oJkyhfB7uQFXqWkTuqur"},
+				{ClusterID: "2", AddressHash: "XhqJaPve64tjUjootwQDjghyJ7RkBxVBYy"},
+				{ClusterID: "2", AddressHash: "XdJPEBAEckf5JVkcN24Eh84S39Az4hwErN"},
+			},
+			wantMapLen: 5,
+			wantErr:    false,
+		},
+		{
+			clusters: []ExternalClusterItem{
+				{ClusterID: "1", AddressHash: "XccPicDbg7HhRfFwPU3Z2CFVpHSYAhuxtu"},
+				{ClusterID: "2", AddressHash: "XggCE7j3QozdXavADMLVEmREPvKXKQexqP"},
+			},
+			wantErr:     true,
+			wantErrType: ErrShallowCluster,
+		},
+		{
+			clusters:    oneThousandAndOneAddresses,
+			wantErr:     true,
+			wantErrType: ErrTooManyAddresses,
+		},
+	}
+	for _, tt := range tests {
+		addresses, err := validateAddresses(dbHandle, tt.clusters)
+		if tt.wantErr {
+			require.Error(t, err)
+			if tt.wantErrType != nil {
+				require.ErrorIs(t, err, tt.wantErrType)
+			}
+		} else {
+			require.NoError(t, err)
+			require.Equal(t, tt.wantMapLen, len(addresses))
+		}
+	}
+}
+
+func TestImportCluster(t *testing.T) {
+	testhelper.SkipIfNotCI(t)
+	db.SetupDB(t, dbHandle, testhelper.UseBlockFile)
+
+	type args struct {
+		clusters []ExternalClusterItem
+		userID   string
+	}
+	tests := []struct {
+		args    args
+		wantErr bool
+	}{
+		{
+			args:    args{userID: ""},
+			wantErr: true,
+		},
+		{
+			args: args{
+				clusters: []ExternalClusterItem{
+					{ClusterID: "1", AddressHash: "XccPicDbg7HhRfFwPU3Z2CFVpHSYAhuxtu"},
+					{ClusterID: "2", AddressHash: "XggCE7j3QozdXavADMLVEmREPvKXKQexqP"},
+					{ClusterID: "1", AddressHash: "Xu4ebhFCAp8vq8oJkyhfB7uQFXqWkTuqur"},
+					{ClusterID: "2", AddressHash: "XhqJaPve64tjUjootwQDjghyJ7RkBxVBYy"},
+					{ClusterID: "2", AddressHash: "XdJPEBAEckf5JVkcN24Eh84S39Az4hwErN"},
+				},
+				userID: "0x1",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		err := ImportCluster(dbHandle, tt.args.clusters, tt.args.userID)
+		if tt.wantErr {
+			require.Error(t, err)
+		} else {
+			require.NoError(t, err)
+		}
+	}
+}
