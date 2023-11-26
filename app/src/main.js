2817a46f6bdb4bc50358b8fa119243eb88285aea
Michael Ziegler <michael.h.ziegler@ntnu.no>
2023-11-26 22:29:46 +0100

    add providers

--- app/src/main.js
+++ app/src/main.js
@@ -12,9 +12,16 @@ import wikiapiConfig from './plugins/wikiAPI';
 const app = createApp(App);
 
 app.use(vuetify).use(router).use(store);
+
+// Todo after switching fully to composition api, check if this can be removed
 app.config.globalProperties.ory = oryConfig;
 app.config.globalProperties.dakar = dakarConfig.setup(app.config.globalProperties);
 app.config.globalProperties.wikiapi = wikiapiConfig.setup(app.config.globalProperties).default;
+
+// Provide global variables here, so they can be later injected
+app.provide('ory', oryConfig);
+app.provide('dakar', dakarConfig.setup(app.config.globalProperties));
+app.provide('wikiapi', wikiapiConfig.setup(app.config.globalProperties).default);
 
 await router.isReady();
 app.mount('#app');
