16d412b2b5654f024a69dec91789e7e4661ced97
Michael Ziegler <michael.h.ziegler@ntnu.no>
2023-11-28 18:39:50 +0100

    remove global property injection

--- app/src/main.js
+++ app/src/main.js
@@ -13,11 +13,6 @@ const app = createApp(App);
 
 app.use(vuetify).use(router).use(store);
 
-// Todo after switching fully to composition api, check if this can be removed
-app.config.globalProperties.ory = oryConfig;
-app.config.globalProperties.dakar = dakarConfig.setup(app.config.globalProperties);
-app.config.globalProperties.wikiapi = wikiapiConfig.setup(app.config.globalProperties).default;
-
 // Provide global variables here, so they can be later injected
 app.provide('ory', oryConfig);
 app.provide('dakar', dakarConfig.setup(app.config.globalProperties));
