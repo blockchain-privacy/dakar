90c06e8fefa433a9dceb882a43014541fd6d50b8
Michael Ziegler <michael.h.ziegler@ntnu.no>
2023-12-19 11:47:18 +0100

    refactor cluster activity tool to more efficient

--- backend/cmd/query/clusterActivity.go
+++ backend/cmd/query/clusterActivity.go
@@ -7,18 +7,73 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
-
-type clusterActivity struct {
-	uid              string
-	transactionCount int
-}
 
 func doExportClusterActivity(dgraph external.Database, fileName string) {
 	addresses, err := analytics.GetAllSingleAddresses(dgraph)
 	if err != nil {
 		warn(err)
 		return
+	}
+	fmt.Println("number of addresses", len(addresses))
+
+	results := map[int]int{}
+
+	// handle addresses
+	const step = 200
+	var offset = 0
+	addressCount := len(addresses)
+	now := time.Now()
+	for {
+		upperBound := offset + step
+		if upperBound > addressCount {
+			upperBound = addressCount
+		}
+
+		counts, err := analytics.GetTransactionCountPerAddresses(dgraph, addresses[offset:upperBound])
+		if err != nil {
+			warn(err)
+			return
+		}
+
+		for _, c := range counts {
+			results[c]++
+		}
+
+		offset += step
+
+		if offset >= addressCount {
+			break
+		}
+
+		if offset%1000 == 0 {
+			timePerAddress := time.Since(now) / time.Duration(offset)
+			fmt.Printf("received counts for %d addresses. %v/a\n", offset, timePerAddress)
+		}
+	}
+
+	// handle clusters
+	clusters, err := analytics.GetAllFMIClusters(dgraph)
+	if err != nil {
+		warn(err)
+		return
+	}
+
+	fmt.Println("number of clusters", len(clusters))
+
+	for i, c := range clusters {
+		txCount, err := analytics.GetTransactionCountPerCluster(dgraph, c)
+		if err != nil {
+			warn(err)
+			return
+		}
+
+		results[txCount]++
+
+		if i%1000 == 0 {
+			fmt.Printf("received counts for %d clusters\n", i)
+		}
 	}
 
 	f, err := os.Create(fileName)
@@ -37,63 +92,16 @@ func doExportClusterActivity(dgraph external.Database, fileName string) {
 	w := csv.NewWriter(f)
 	defer w.Flush()
 
-	// handle addresses
-	activities := make([]clusterActivity, len(addresses))
-	for i, a := range addresses {
-		txCount, err := analytics.GetTransactionCountPerAddress(dgraph, a)
-		if err != nil {
-			warn(err)
-			return
-		}
-		activities[i] = clusterActivity{uid: a, transactionCount: txCount}
-
-		if i%100000 == 0 {
-			fmt.Printf("received counts for %d addresses", i)
-		}
-	}
-
-	for _, a := range activities {
-		// column 1: cluster or address uid
-		// column 2: entity type
-		// column 3: number of transactions created by this entity
-		line := []string{a.uid, "address", strconv.Itoa(a.transactionCount)}
-
-		if err := w.Write(line); err != nil {
-			warn(err, "msg", "error writing record to file")
-			return
-		}
-	}
-
-	w.Flush()
-
-	// handle clusters
-	clusters, err := analytics.GetAllFMIClusters(dgraph)
-	if err != nil {
-		warn(err)
+	// header
+	if err := w.Write([]string{"number of transactions", "cluster count"}); err != nil {
+		warn(err, "msg", "error writing record to file")
 		return
 	}
 
-	activities = make([]clusterActivity, len(clusters))
-	for i, c := range clusters {
-		txCount, err := analytics.GetTransactionCountPerCluster(dgraph, c)
-		if err != nil {
-			warn(err)
-			return
-		}
-
-		activities[i] = clusterActivity{uid: c, transactionCount: txCount}
-
-		if i%100000 == 0 {
-			fmt.Printf("received counts for %d clusters", i)
-		}
-	}
-
-	for _, a := range activities {
-		// column 1: cluster or address uid
-		// column 2: entity type
-		// column 3: number of transactions created by this entity
-		line := []string{a.uid, "cluster", strconv.Itoa(a.transactionCount)}
-
+	for k, v := range results {
+		// column 1: number of transactions
+		// column 2: cluster count
+		line := []string{strconv.Itoa(k), strconv.Itoa(v)}
 		if err := w.Write(line); err != nil {
 			warn(err, "msg", "error writing record to file")
 			return
