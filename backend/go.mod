664208812e54e79dc5a1104cb1712a18314492b2
Michael Ziegler <michael.h.ziegler@ntnu.no>
2024-04-14 18:06:23 +0200

    remove btcd tests

--- backend/go.mod
+++ backend/go.mod
@@ -4,7 +4,6 @@ go 1.22
 
 require (
 	github.com/btcsuite/btcd v0.24.0
-	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0
 	github.com/dgraph-io/dgo/v230 v230.0.1
 	github.com/dgraph-io/ristretto v0.1.1
 	github.com/ory/kratos-client-go v1.1.0
@@ -19,9 +18,8 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.3 // indirect
 	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
-	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
-	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
--- backend/jsonrpc/jsonrpc_test.go
+++ backend/jsonrpc/jsonrpc_test.go
@@ -1,55 +1,8 @@
 package jsonrpc
 
 import (
-	cli "backend/cmd/cliutil"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/rpcclient"
 	"testing"
 )
-
-func getOldRPC() (*rpcclient.Client, error) {
-	rpcEndpoint, err := cli.BuildEndpoint("0.0.0.0", 9998)
-	if err != nil {
-		return nil, err
-	}
-
-	connection := rpcclient.ConnConfig{
-		Host:                rpcEndpoint,
-		User:                "rpc1user",
-		Pass:                "1234pass",
-		DisableConnectOnNew: true,
-		DisableTLS:          true,
-		HTTPPostMode:        true,
-	}
-
-	client, err := rpcclient.New(&connection, nil)
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
-
-func getOldBatchRPC() (*rpcclient.Client, error) {
-	rpcEndpoint, err := cli.BuildEndpoint("0.0.0.0", 9998)
-	if err != nil {
-		return nil, err
-	}
-
-	connection := rpcclient.ConnConfig{
-		Host:                rpcEndpoint,
-		User:                "rpc1user",
-		Pass:                "1234pass",
-		DisableConnectOnNew: true,
-		DisableTLS:          true,
-		HTTPPostMode:        true,
-	}
-
-	client, err := rpcclient.NewBatch(&connection)
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
 
 func BenchmarkGetBlockCount(b *testing.B) {
 	rpc := NewDashClient("0.0.0.0:9998", "rpc1user", "1234pass", nil)
@@ -63,39 +16,9 @@ func BenchmarkGetBlockCount(b *testing.B) {
 	}
 }
 
-func BenchmarkGetBlockCount_old(b *testing.B) {
-	rpc, err := getOldRPC()
-	if err != nil {
-		b.Error(err)
-		return
-	}
-	for range b.N {
-		_, err := rpc.GetBlockCount()
-		if err != nil {
-			b.Error(err)
-			return
-		}
-	}
-}
-
 func BenchmarkGetBlockHash(b *testing.B) {
 	rpc := NewDashClient("0.0.0.0:9998", "rpc1user", "1234pass", nil)
 
-	for i := range b.N {
-		_, err := rpc.GetBlockHash(100000 + int64(i))
-		if err != nil {
-			b.Error(err)
-			return
-		}
-	}
-}
-
-func BenchmarkGetBlockHash_old(b *testing.B) {
-	rpc, err := getOldRPC()
-	if err != nil {
-		b.Error(err)
-		return
-	}
 	for i := range b.N {
 		_, err := rpc.GetBlockHash(100000 + int64(i))
 		if err != nil {
@@ -286,78 +209,12 @@ func BenchmarkGetRawTransactionVerbose(b *testing.B) {
 		}
 	}
 }
-func BenchmarkGetRawTransactionVerbose_old(b *testing.B) {
-	rpc, err := getOldRPC()
-	if err != nil {
-		b.Error(err)
-		return
-	}
-
-	hashes := make([]*chainhash.Hash, len(testTransactions4))
-	for i, tx := range testTransactions4 {
-		h, err := chainhash.NewHashFromStr(tx)
-		if err != nil {
-			b.Error(err)
-			return
-		}
-
-		hashes[i] = h
-	}
-
-	for i := range b.N {
-		_, err := rpc.GetRawTransactionVerbose(hashes[i%len(hashes)])
-		if err != nil {
-			b.Error(err)
-			return
-		}
-	}
-}
-
 func BenchmarkGetRawTransactionVerboseSimulatedBatch(b *testing.B) {
 	rpc := NewDashClient("0.0.0.0:9998", "rpc1user", "1234pass", nil)
 
 	for range b.N {
 		for y := range len(testTransactions40) {
 			_, err := rpc.GetRawTransactionVerbose(testTransactions40[y])
-			if err != nil {
-				b.Error(err)
-				return
-			}
-		}
-	}
-}
-
-func BenchmarkGetRawTransactionVerboseBatch_old(b *testing.B) {
-	rpc, err := getOldBatchRPC()
-	if err != nil {
-		b.Error(err)
-		return
-	}
-
-	hashes := make([]*chainhash.Hash, len(testTransactions40))
-	for i, tx := range testTransactions40 {
-		h, err := chainhash.NewHashFromStr(tx)
-		if err != nil {
-			b.Error(err)
-			return
-		}
-
-		hashes[i] = h
-	}
-
-	for range b.N {
-		var futures []rpcclient.FutureGetRawTransactionVerboseResult
-		for y := range len(hashes) {
-			futures = append(futures, rpc.GetRawTransactionVerboseAsync(hashes[y]))
-		}
-
-		if err := rpc.Send(); err != nil {
-			b.Error(err)
-			return
-		}
-
-		for _, f := range futures {
-			_, err := f.Receive()
 			if err != nil {
 				b.Error(err)
 				return
