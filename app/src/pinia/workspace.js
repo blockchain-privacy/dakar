291da8fc8531e591cdf6b1a6148832507d028ca3
Michael Ziegler <michael.h.ziegler@ntnu.no>
2024-05-07 23:15:37 +0200

    add Set() to workspace store to enable loading multiple nodes at once

--- app/src/pinia/workspace.js
+++ app/src/pinia/workspace.js
@@ -4,6 +4,8 @@ export const useWorkspaceStore = defineStore('workspace', {
 	state: () => ({
 		// If the workspace is loaded, this variable is being watched. When the value changes the item is loaded into the workspace.
 		workspaceNode: null,
+		// If the workspace is loaded, this variable is being watched. Holds all node IDs which can be loaded into the workspace.
+		workspaceNodes: new Set(),
 		// Is set to true as soon as the workspace component is mounted and set to false when it is unmounted
 		isWorkspaceActive: false,
 	}),
@@ -16,6 +18,12 @@ export const useWorkspaceStore = defineStore('workspace', {
 		},
 		setWorkspaceActive(active) {
 			this.isWorkspaceActive = active;
+		},
+		addNodeToSet(payload) {
+			this.workspaceNodes.add(payload);
+		},
+		removeNodeFromSet(payload) {
+			this.workspaceNodes.delete(payload);
 		},
 	},
 });
