110b03c35d5122c54a026bf142eae5d3af958e45
Michael Ziegler <michael.h.ziegler@ntnu.no>
2024-08-15 12:11:33 +0200

    revert color change

--- app/src/utilities/index.js
+++ app/src/utilities/index.js
@@ -325,11 +325,11 @@ export function plural(subject, count) {
 export function getColorMap() {
 	const colorMap = new Map();
 
-	colorMap.set('origin', '#EFE341');
-	colorMap.set('mixing', '#E59E07');
-	colorMap.set('destination', '#D45D05');
-	colorMap.set('collateral creation', '#55B3E8');
-	colorMap.set('collateral payment', '#0C71B1');
+	colorMap.set('origin', '#D55E00');
+	colorMap.set('mixing', '#56B4E9');
+	colorMap.set('destination', '#0072B2');
+	colorMap.set('collateral creation', '#E69F00');
+	colorMap.set('collateral payment', '#009E73');
 
 	return colorMap;
 }
