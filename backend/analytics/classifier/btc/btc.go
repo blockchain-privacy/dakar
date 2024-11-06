c52b64c10374b5633dae3e3ceaca5b61bb52a43b
Michael Ziegler <michael.h.ziegler@ntnu.no>
2024-11-06 19:28:19 +0100

    do classification of origins and destinations after whirlpool mixing transactions have been set

--- backend/analytics/classifier/btc/btc.go
+++ backend/analytics/classifier/btc/btc.go
@@ -48,30 +48,35 @@ func Iterate(ctx context.Context, c external.Database, from int64, to int64) (bo
 		}
 	}
 
-	// step 2: set the privacy type of transactions by analyzing the connected transactions.
+	if len(whirlpoolMixingUIDs) > 0 {
+		// step 2.1: get potential whirlpool origin transactions
+		potWhirlpoolOrigins, originsToMixingMap, err := btc.GetPotentialWhirlpoolMixingTransactions(ctx, c, whirlpoolMixingUIDs)
+		if err != nil {
+			return false, err
+		}
 
-	// step 2.1: set the privacy type of wasabi destination, wasabi origin and whirlpool destination
-	// transactions by analyzing the connected transactions. Potential whirlpool origin transactions are
-	// only returned in this step and not set directly. The potential whirlpool origin transactions are
-	// further evaluated in the next step
-	potWhirlpoolOrigins, originsToMixingMap, err := btc.ClassifyDestinationAndOriginsByBlock(ctx, c, from, to, whirlpoolMixingUIDs)
-	if err != nil {
-		return false, err
-	}
-
-	// step 2.2: whirlpool mixing transactions must be connected to at least one whirlpool origin transaction.
-	// Whirlpool origin transactions have a high chance to be misclassified,
-	// therefore classify both mixing and origin transactions only if they are connnected to each other.
-	if len(potWhirlpoolOrigins) > 0 {
-		// - get all unclassified transactions which are connected to the potential whirlpool mixing transactions
-		// - from the results, check if any transaction can be classified as a whirlpool origin transaction
-		// - persist classifications of all origin-mixing pairs
-		classfiedTransactions := classifyWhirlpoolOriginTransactions(potWhirlpoolOrigins, originsToMixingMap)
-		if len(classfiedTransactions) > 0 {
-			if err = db.UpdateTransactions(ctx, c, classfiedTransactions); err != nil {
-				return false, err
+		// step 2.2: whirlpool mixing transactions must be connected to at least one whirlpool origin transaction.
+		// Whirlpool origin transactions have a high chance to be misclassified,
+		// therefore classify both mixing and origin transactions only if they are connnected to each other.
+		if len(potWhirlpoolOrigins) > 0 {
+			// - get all unclassified transactions which are connected to the potential whirlpool mixing transactions
+			// - from the results, check if any transaction can be classified as a whirlpool origin transaction
+			// - persist classifications of all origin-mixing pairs
+			classfiedTransactions := classifyWhirlpoolOriginTransactions(potWhirlpoolOrigins, originsToMixingMap)
+			if len(classfiedTransactions) > 0 {
+				if err = db.UpdateTransactions(ctx, c, classfiedTransactions); err != nil {
+					return false, err
+				}
 			}
 		}
+	}
+
+	// step 3: set the privacy type of
+	// - wasabi 2.0 origin transactions
+	// - wasabi 2.0 destination transactions
+	// - whirlpool destination transactions
+	if err = btc.ClassifyDestinationAndOriginsByBlock(ctx, c, from, to); err != nil {
+		return false, err
 	}
 
 	return true, nil
--- backend/db/analytics/classifier/btc/btc.go
+++ backend/db/analytics/classifier/btc/btc.go
@@ -11,14 +11,16 @@ import (
 	"strconv"
 )
 
-// ClassifyDestinationAndOriginsByBlock sets the transaction type for destination transactions in the given block range and
-// the origin transaction type for all transactions which are connected to mixing
-// transactions in this block.
+// ClassifyDestinationAndOriginsByBlock sets the transaction type for
+// - wasabi 2.0 origin transactions
+// - wasabi 2.0 destination transactions
+// - whirlpool destination transactions
+// in the given block range.
 // Destination transactions are transactions which are connected to outputs of mixing transactions and at the
 // same time are not mixing transactions themselves. Origin transactions are transactions which are connected to
 // inputs of mixing transactions and at the same time are not mixing transactions themselves.
 func ClassifyDestinationAndOriginsByBlock(ctx context.Context, c external.Database,
-	fromBlockID int64, toBlockID int64, whirlpoolMixingUIDs []string) ([]db.Transaction, map[string][]string, error) {
+	fromBlockID int64, toBlockID int64) error {
 	query := `query Q($from:int,$to:int,$whirlpoolMixingUIDs:string) {
 				b as var(func: between(id, $from, $to))
 				var(func: uid(b))@cascade{
@@ -47,7 +49,34 @@ func ClassifyDestinationAndOriginsByBlock(ctx context.Context, c external.Databa
 						}
 					}
 				}
+			  }`
 
+	req := &api.Request{
+		Query: query,
+		Vars:  map[string]string{"$from": strconv.FormatInt(fromBlockID, 10), "$to": strconv.FormatInt(toBlockID, 10)},
+		Mutations: []*api.Mutation{
+			{
+				Cond:      "@if(gt(len(whirlpoolDestinations), 0))",
+				SetNquads: []byte("uid(whirlpoolDestinations) <Transaction.type> \"" + constants.TypeWhirlpoolDestination + "\" ."),
+			},
+			{
+				Cond:      "@if(gt(len(wasabi2Origins), 0))",
+				SetNquads: []byte("uid(wasabi2Origins) <Transaction.type> \"" + constants.TypeWasabi2Origin + "\" ."),
+			},
+			{
+				Cond:      "@if(gt(len(wasabi2Destinations), 0))",
+				SetNquads: []byte("uid(wasabi2Destinations) <Transaction.type> \"" + constants.TypeWasabi2Destination + "\" ."),
+			},
+		},
+		CommitNow: true,
+	}
+
+	return db.MutationWithRetry(ctx, c, req)
+}
+
+func GetPotentialWhirlpoolMixingTransactions(ctx context.Context, c external.Database,
+	whirlpoolMixingUIDs []string) ([]db.Transaction, map[string][]string, error) {
+	query := `query Q($whirlpoolMixingUIDs:string) {
 				q(func: uid($whirlpoolMixingUIDs)){
 					uid
 					tx_inputs{
@@ -74,23 +103,8 @@ func ClassifyDestinationAndOriginsByBlock(ctx context.Context, c external.Databa
 			  }`
 
 	req := &api.Request{
-		Query: query,
-		Vars: map[string]string{"$from": strconv.FormatInt(fromBlockID, 10),
-			"$to": strconv.FormatInt(toBlockID, 10), "$whirlpoolMixingUIDs": db.CreateCommaArray(whirlpoolMixingUIDs)},
-		Mutations: []*api.Mutation{
-			{
-				Cond:      "@if(gt(len(whirlpoolDestinations), 0))",
-				SetNquads: []byte("uid(whirlpoolDestinations) <Transaction.type> \"" + constants.TypeWhirlpoolDestination + "\" ."),
-			},
-			{
-				Cond:      "@if(gt(len(wasabi2Origins), 0))",
-				SetNquads: []byte("uid(wasabi2Origins) <Transaction.type> \"" + constants.TypeWasabi2Origin + "\" ."),
-			},
-			{
-				Cond:      "@if(gt(len(wasabi2Destinations), 0))",
-				SetNquads: []byte("uid(wasabi2Destinations) <Transaction.type> \"" + constants.TypeWasabi2Destination + "\" ."),
-			},
-		},
+		Query:     query,
+		Vars:      map[string]string{"$whirlpoolMixingUIDs": db.CreateCommaArray(whirlpoolMixingUIDs)},
 		CommitNow: true,
 	}
 
