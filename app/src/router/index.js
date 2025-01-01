4c34f6ae434ec0e27b276f4c524dca9066998075
Michael Ziegler <michael.h.ziegler@ntnu.no>
2025-01-01 15:33:37 +0100

    shorten no results message

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -195,7 +195,7 @@ export const router = createRouter({
 			props: {
 				default: true,
 				title: 'No results found!',
-				description: 'Your search query did not return any results. Either navigate back or click below to get back to the entry page.',
+				description: 'Your search query did not return any results.',
 				imageSource: NoResultsImg,
 			},
 		},
