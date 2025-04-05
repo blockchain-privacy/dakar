e7248e793373bf301ec15db22f5dfbacf7c66711
Michael Ziegler <michael.h.ziegler@ntnu.no>
2025-04-05 20:06:17 +0200

    process heuristics in parallel

--- backend/cmd/query/heuristic.go
+++ backend/cmd/query/heuristic.go
@@ -12,6 +12,7 @@ import (
 	"github.com/qrest/gomisc/serror"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -68,6 +69,96 @@ func doHeuristicAnalysis(ctx context.Context, dgraph external.Database, g *graph
 	wrapper.SetGraph(g)
 
 	lookbackDurations := []int{12, 24, 48}
+
+	workContext, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// work on jobs
+	jobs := make(chan analytics.NodeWithHash, 100)
+	results := make(chan []string, 100)
+	wg := sync.WaitGroup{}
+	for range 7 {
+		wg.Add(1)
+		go func(jobs <-chan analytics.NodeWithHash, results chan<- []string) {
+			defer wg.Done()
+			for destination := range jobs {
+				select {
+				case <-workContext.Done():
+					return
+				default:
+				}
+				var sum int64
+				for _, t := range destination.Outputs {
+					sum += t.Amount
+				}
+
+				// column 1: transaction hash
+				// column 2: transaction timestamp
+				// column 3: input count (only mixing)
+				// column 4: input amount
+				// column 5: number of clusters (reverse lookup 12h)
+				// column 6: number of origins (reverse lookup 12h)
+				// column 7: number of clusters (reverse lookup 24h)
+				// column 8: number of origins (reverse lookup 24h)
+				// column 9: number of clusters (reverse lookup 48h)
+				// column 10: number of origins (reverse lookup 48h)
+				// column 11: number of clusters (one source 12h)
+				// column 12: number of origins (one source 12h)
+				// column 13: number of clusters (one source 24h)
+				// column 14: number of origins (one source 24h)
+				// column 15: number of clusters (one source 48h)
+				// column 16: number of origins (one source 48h)
+				line := []string{destination.Hash, destination.Block[0].TS.Format(time.RFC3339),
+					strconv.Itoa(len(destination.Outputs)), strconv.FormatInt(sum, 10)}
+				for _, txHeuristic := range txHeuristics {
+					for _, duration := range lookbackDurations {
+						if err = txHeuristic.SetConfig(dbh.Options{Parameter: strconv.Itoa(duration),
+							TransactionHash: destination.Hash}); err != nil {
+							cancel()
+							warn(err)
+							return
+						}
+
+						clusters, err := txHeuristic.Exec(ctx, dgraph, wrapper, destination.UID)
+						if err != nil {
+							cancel()
+							warn(err)
+							return
+						}
+
+						var originCount int
+						for _, cluster := range clusters {
+							originCount += len(cluster.Results)
+						}
+
+						line = append(line, strconv.Itoa(len(clusters)), strconv.Itoa(originCount))
+					}
+				}
+
+				results <- line
+			}
+		}(jobs, results)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		for line := range results {
+			select {
+			case <-workContext.Done():
+				return
+			default:
+			}
+			if err := w.Write(line); err != nil {
+				cancel()
+				warn(err, "msg", "error writing record to file")
+				return
+			}
+		}
+		// signal that all results got processed
+		close(done)
+	}()
+
+	// create jobs
 	for i := 0; ; i += step {
 		now := time.Now()
 		destinations, err := analytics.GetPrivacyTransactionsWithHash(ctx, dgraph, step, i, transactionType, mixingTxType)
@@ -88,56 +179,13 @@ func doHeuristicAnalysis(ctx context.Context, dgraph external.Database, g *graph
 				continue
 			}
 
-			var sum int64
-			for _, t := range destination.Outputs {
-				sum += t.Amount
-			}
-
-			// column 1: transaction hash
-			// column 2: transaction timestamp
-			// column 3: input count (only mixing)
-			// column 4: input amount
-			// column 5: number of clusters (reverse lookup 12h)
-			// column 6: number of origins (reverse lookup 12h)
-			// column 7: number of clusters (reverse lookup 24h)
-			// column 8: number of origins (reverse lookup 24h)
-			// column 9: number of clusters (reverse lookup 48h)
-			// column 10: number of origins (reverse lookup 48h)
-			// column 11: number of clusters (one source 12h)
-			// column 12: number of origins (one source 12h)
-			// column 13: number of clusters (one source 24h)
-			// column 14: number of origins (one source 24h)
-			// column 15: number of clusters (one source 48h)
-			// column 16: number of origins (one source 48h)
-			line := []string{destination.Hash, ts.Format(time.RFC3339),
-				strconv.Itoa(len(destination.Outputs)), strconv.FormatInt(sum, 10)}
-			for _, txHeuristic := range txHeuristics {
-				for _, duration := range lookbackDurations {
-					if err = txHeuristic.SetConfig(dbh.Options{Parameter: strconv.Itoa(duration),
-						TransactionHash: destination.Hash}); err != nil {
-						warn(err)
-						return
-					}
-
-					clusters, err := txHeuristic.Exec(ctx, dgraph, wrapper, destination.UID)
-					if err != nil {
-						warn(err)
-						return
-					}
-
-					var originCount int
-					for _, cluster := range clusters {
-						originCount += len(cluster.Results)
-					}
-
-					line = append(line, strconv.Itoa(len(clusters)), strconv.Itoa(originCount))
-				}
-			}
-
-			if err := w.Write(line); err != nil {
-				warn(err, "msg", "error writing record to file")
+			select {
+			case <-workContext.Done():
 				return
+			case jobs <- destination:
+			default:
 			}
+
 		}
 
 		info("execution duration", "duration/transactions",
@@ -147,4 +195,12 @@ func doHeuristicAnalysis(ctx context.Context, dgraph external.Database, g *graph
 			break
 		}
 	}
+
+	close(jobs)
+	// wait for workers to finish
+	wg.Wait()
+	// results channel can now be closed
+	close(results)
+	// wait until all results got processed
+	<-done
 }
