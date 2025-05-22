84cc467c49eee5d305069a02bfa7eebd9af44c3e
Michael Ziegler <michael.h.ziegler@ntnu.no>
2025-05-22 17:49:02 +0200

    Revert "use tanh for scaling"
    This reverts commit b0a3a0b38954b4d8f916061d44890921cc5a32a5.

--- backend/analytics/graph/fingerprint.go
+++ backend/analytics/graph/fingerprint.go
@@ -81,11 +81,14 @@ func SpendingFingerprint(g *ReversibleGraph, uid string) ([]FingerPrint, error) 
 
 		dist := calcChamferDistance(g, rootTx, txNode)
 		// only consider average distances of less or equal than 24h
-		if dist > 24 {
+		if dist > 86400 {
 			continue
 		}
 
-		fingerprints = append(fingerprints, FingerPrint{TransactionUID: txNode.String(), Score: dist})
+		fingerprints = append(fingerprints, FingerPrint{
+			TransactionUID: txNode.String(),
+			Score:          dist,
+		})
 		sort.Slice(fingerprints, func(i, j int) bool {
 			return fingerprints[i].Score > fingerprints[j].Score
 		})
@@ -140,8 +143,7 @@ func chamferDistanceOneSided(arr1, arr2 []float64) float64 {
 				minDistance = distance
 			}
 		}
-
-		totalDistance += math.Tanh(minDistance/3600/2-2) + 1
+		totalDistance += minDistance
 	}
 
 	return totalDistance / float64(len(arr1))
