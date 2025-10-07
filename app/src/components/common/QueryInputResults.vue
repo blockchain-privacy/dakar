7aabe6bb05940deb8b6d71893dde933b724ec152
Michael Ziegler <michael.h.ziegler@ntnu.no>
2025-10-07 18:14:58 +0200

    add SPDX identifiers to vue source code

--- app/src/components/common/QueryInputResults.vue
+++ app/src/components/common/QueryInputResults.vue
@@ -1,3 +1,7 @@
+<!-- SPDX-FileCopyrightText: 2025 Michael Ziegler <michael.h.ziegler@ntnu.no> -->
+<!-- SPDX-FileCopyrightText: 2025 Mariusz Nowostawski <mariusz.nowostawski@ntnu.no> -->
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+
 <template>
   <v-list min-width="270px">
     <v-list-item
