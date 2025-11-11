3c7676f5e78517e7ddff54701754bf1d95fd310b
Michael Ziegler <michael.h.ziegler@ntnu.no>
2025-11-11 22:11:06 +0100

    status page: use error message component, instead of pushing errors to msg box

---
+++ app/src/components/common/ErrorMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert
+    v-if="text"
+    :text="text"
+    type="error"
+    variant="tonal"
+  />
+</template>
+
+<script setup>
+
+defineProps({text: {type: String, required: true}});
+
+</script>
+<style scoped>
+
+</style>
